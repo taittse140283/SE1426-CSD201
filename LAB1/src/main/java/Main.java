f985ed83a229dd3fea2af4d46aded53ee66ed47f
Thai Tieu Minh <tieuminh2510@gmail.com>
2020-06-01 11:29:35 +0700

    Update Hello World

--- LAB1/src/main/java/Main.java
+++ LAB1/src/main/java/Main.java
@@ -14,6 +14,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.print("Hello World");
         // TODO code application logic here
     }
     
