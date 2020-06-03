d42ae0ff720ee7910b174517d91032970830ccdc
BachDuyHoang <bachduyhoang2k@gmail.com>
2020-06-03 10:46:58 +0700

    add

--- LAB1/src/main/java/Main.java
+++ LAB1/src/main/java/Main.java
@@ -15,6 +15,7 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.print("Hello World");
+        System.out.println("helekrekwlr");
         // TODO code application logic here
     }
     
