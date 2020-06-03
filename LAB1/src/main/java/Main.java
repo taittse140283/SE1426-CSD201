7d4301530875ada1d58cca7a748b1d137d586e28
luongse <luonghb24@gmail.com>
2020-06-03 10:46:07 +0700

    test commit hehehe

--- LAB1/src/main/java/Main.java
+++ LAB1/src/main/java/Main.java
@@ -16,6 +16,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Hello World");
         // TODO code application logic here
+        System.out.println(" hehehehe");
     }
     
 }
