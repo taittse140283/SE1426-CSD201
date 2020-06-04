4b7b348f3e8ad98f65cf91bdbf58faa19dfaba74
luongse <luonghb24@gmail.com>
2020-06-04 13:53:53 +0700

    update class Entry

--- LAB1/src/main/java/lab1/Entry.java
+++ LAB1/src/main/java/lab1/Entry.java
@@ -9,10 +9,46 @@ package lab1;
  *
  * @author Admin
  */
+import java.util.Scanner;
 public class Entry {
-    public static void main(String[] args) {
-        System.out.println("test file");
-        System.out.println("^^");
+    Scanner sc= new Scanner(System.in);
+    private String _email; // entry variable
+    private int _point;    // entry variable
+    public Entry(String email, int point){ //constructor have parametter;
+        _email=email;
+        _point=point;
+    }
+    public Entry(){}//constructor have no parametters
+
+    public String getEmail() {
+        return _email;
+    }
+
+    public void setEmail(String _email) {
+        this._email = _email;
+    }
+
+    public int getPoint() {
+        return _point;
+    }
+
+    public void setPoint(int _point) {
+        this._point = _point;
+    }
+    
+    // Enter input information of player include email and point;
+    public void input(){
+        System.out.println("Please enter the email:");
+        _email=sc.nextLine();
+        System.out.println("Enter the your point:");
+        _point=sc.nextInt();
         
     }
+    //method to print the display
+     public String toString(){
+        return "(" +_email + "," +_point +")" ;
+    }
+    
+    
+     
 }
