3978a466dde9ce0adda29ea363e1efeab5c269a6
luongse <luonghb24@gmail.com>
2020-06-04 07:17:30 +0700

    test up file

---
+++ LAB1/src/main/java/lab1/Entry.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+/**
+ *
+ * @author Admin
+ */
+public class Entry {
+    public static void main(String[] args) {
+        System.out.println("test file");
+    }
+}
