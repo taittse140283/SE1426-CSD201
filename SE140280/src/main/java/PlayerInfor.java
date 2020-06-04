0495de4b713fb74a3809eca535313a2dec9621e4
ZunPeter <ZunPeter@DESKTOP-D9HH4VK>
2020-06-04 14:15:17 +0700

    recommit

--- SE140280/src/main/java/PlayerInfor.java
+++ SE140280/src/main/java/PlayerInfor.java
@@ -9,4 +9,28 @@
  * @author ZunPeter
  */
 public class PlayerInfor {
+    private String email;
+    private int point;
+
+    public PlayerInfor(String email, int point) {
+        this.email = email;
+        this.point = point;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+   
 }
