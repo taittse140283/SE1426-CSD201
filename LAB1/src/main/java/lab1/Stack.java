55b905496a4e426c416112e4b18cddd887f90d86
luongse <luonghb24@gmail.com>
2020-06-04 14:12:23 +0700

    create interface Stack

---
+++ LAB1/src/main/java/lab1/Stack.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+/**
+ *
+ * @author Admin
+ */
+public interface Stack {
+    public void add(String new_email);
+    public void delete(String email);
+    public void update(String email);
+    public void get(String email);
+    public void getMax(String email);
+    public void deleteMax(String email);
+    
+    
+}
