eb3cfd7f4ea9e2eea2f71fe25143648e05bc5c67
Dell <Dell@SE140363>
2020-06-04 13:55:10 +0700

    Class Infor

---
+++ LAB1/src/main/java/Lab/Infor.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Dell
+ */
+public class Infor {
+    private String email;
+    private int point;
+}
