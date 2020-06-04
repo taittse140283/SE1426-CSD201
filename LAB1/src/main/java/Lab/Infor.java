ed39638dfa6c5f326e32c9eba1cae5839c0346cf
Dell <Dell@SE140363>
2020-06-04 14:00:53 +0700

    Class Infor

--- LAB1/src/main/java/Lab/Infor.java
+++ LAB1/src/main/java/Lab/Infor.java
@@ -14,4 +14,39 @@ import java.util.Scanner;
 public class Infor {
     private String email;
     private int point;
+
+    public Infor() {
+    }
+
+    public Infor(String email, int point) {
+        this.email = email;
+        this.point = point;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+    
+    public void input(){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Email:");
+        email= sc.nextLine();
+        System.out.println("Point:");
+        point= Integer.parseInt(sc.nextLine());
+    }
+    public void output(){
+        System.out.println("Emai:" + email + "|" + "Point:" + point);
+    }
 }
