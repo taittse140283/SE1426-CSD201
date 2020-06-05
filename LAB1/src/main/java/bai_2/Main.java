d9beda72b221ecec7531c302d294332b01a1f505
Linh <Linh@SE141095.mshome.net>
2020-06-05 22:31:32 +0700

    Testing GetContentFromWebsite class

--- LAB1/src/main/java/bai_2/Main.java
+++ LAB1/src/main/java/bai_2/Main.java
@@ -10,21 +10,29 @@ package bai_2;
  * @author Linh
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         
         //Kiem tra cac function cua stack
-        Stack s = new Stack();
-        s.push("<html>");
-        s.push("<a>");
-        s.push("<body>");
-        s.push("<img>");
-        s.push("<style>");
-        s.print();
-        System.out.println();
-        s.pop();
-        s.pop();
-        s.pop();
-        s.print();
+//        Stack s = new Stack();
+//        s.push("<html>");
+//        s.push("<a>");
+//        s.push("<body>");
+//        s.push("<img>");
+//        s.push("<style>");
+//        s.print();
+//        System.out.println();
+//        s.pop();
+//        s.pop();
+//        s.pop();
+//        s.print();
+
+        //Testing GetContentFromWebsite
+        String url = "https://vnexpress.net/";
+        String s = null;
+        GetContentFromWebsite g = new GetContentFromWebsite();
+        s = g.readContent(url);
+        for(int i = 0; i < s.length(); i++)
+            System.out.print(s.charAt(i));
        
     }
 }
