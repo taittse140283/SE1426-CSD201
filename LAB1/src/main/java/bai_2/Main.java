c7f27291b88c100c2a2f8621cc3058764a87055b
Linh <Linh@SE141095.mshome.net>
2020-06-05 13:53:58 +0700

    commit

---
+++ LAB1/src/main/java/bai_2/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai_2;
+
+/**
+ *
+ * @author Linh
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        //Kiem tra cac function cua stack
+        Stack s = new Stack();
+        s.push("<html>");
+        s.push("<a>");
+        s.push("<body>");
+        s.push("<img>");
+        s.push("<style>");
+        s.print();
+        System.out.println();
+        s.pop();
+        s.pop();
+        s.pop();
+        s.print();
+       
+    }
+}
