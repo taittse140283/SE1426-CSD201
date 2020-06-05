e25214bdf86e1d5e7dc7b50bde47036bf51a035f
User <User@192.168.52.1>
2020-06-05 15:27:41 +0700

    lấy node đầu và cuối trong list mà không xóa nó đi

--- LAB1/src/main/java/DoublyLinkedList.java
+++ LAB1/src/main/java/DoublyLinkedList.java
@@ -9,49 +9,76 @@
  * @author User
  */
 public class DoublyLinkedList<E> {
-    
+
     //tao lop node
     private class Node<E> {
-        private E data;  // tham chieu den gia tri(du lieu) cua node
-        private Node prev; // tham chieu den node dung truoc trong danh sach(thanh phan lien ket giua cac node thong qua viec chua dia chi cua node dung truoc)
-        private Node next;// tham chieu den node dung sau trong danh sach(thanh phan lien ket giua cac node thong qua viec chua dia chi cua node dung sau)
 
-        public Node(E data, Node prev, Node next) {
+        private E data;  // tham chieu den gia tri(du lieu) cua node
+        private Node<E> prev; // tham chieu den node dung truoc trong danh sach(thanh phan lien ket giua cac node thong qua viec chua dia chi cua node dung truoc)
+        private Node<E> next;// tham chieu den node dung sau trong danh sach(thanh phan lien ket giua cac node thong qua viec chua dia chi cua node dung sau)
+
+        public Node(E data, Node<E> prev, Node<E> next) {
             this.data = data;
             this.prev = prev;
             this.next = next;
         }
-        
+
         public E getElement() {
             return data;
         }
-        
-        public Node getPrev() {
+
+        public Node<E> getPrev() {
             return prev;
         }
-        
-        public Node getNext() {
+
+        public Node<E> getNext() {
             return next;
         }
-        
+
         public void setPrev(Node<E> p) {
             prev = p;
         }
-        
+
         public void setNext(Node<E> n) {
             next = n;
         }
     }
-    
+
     private Node<E> header;
     private Node<E> tralier;
     private int size = 0;
 
     // tao constructor voi danh sach rong
     public DoublyLinkedList() {
-        header = new Node<>(null, null, null);  
+        header = new Node<>(null, null, null);
         tralier = new Node<>(null, header, null);
         header.setNext(tralier);
+    }
+
+    // tra ve so luong cac thanh phan trong danh sach
+    public int size() {
+        return size;
+    }
+
+    //tra ve list rong neu size = 0
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // tra ve node dau tien trong danh sach ma khong remove no
+    public E first() {
+        if (isEmpty()) {
+            return null;
+        }
+        return header.getNext().getElement();
+    }
+    
+    // tra ve node cuoi cung trong danh sach ma khong remove no
+    public E last() {
+        if(isEmpty()) {
+            return null;
+        }
+        return tralier.getPrev().getElement();
     }
     
     
