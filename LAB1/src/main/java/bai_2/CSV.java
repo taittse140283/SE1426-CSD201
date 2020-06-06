d1e7b682f6a41aca641e776b7a53aa027ac9030a
Linh <Linh@SE141095.mshome.net>
2020-06-06 22:56:20 +0700

    Update CSV class

--- LAB1/src/main/java/bai_2/CSV.java
+++ LAB1/src/main/java/bai_2/CSV.java
@@ -5,8 +5,13 @@
  */
 package bai_2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -38,11 +43,40 @@ public class CSV {
     
     //Print to screen for testing
     public void print() {
+        
+        System.out.println("Before sorting by frequence:");
         for (Map.Entry<String, Integer> entry : csv.entrySet()) {
             String key = entry.getKey();
             Integer value = entry.getValue();
-            
-            System.out.println("key: " + key + " - value: " + value);
+            System.out.println("tag: " + key + " - frequences: " + value);
         }
+    }
+    
+    public void sortAndWriteToFile(String fileName) throws FileNotFoundException {
+        
+        //This will sort by value in descending order
+        Map<String, Integer> sortedByFrequences = csv.entrySet().stream()
+            .sorted((Map.Entry.<String, Integer> comparingByValue().reversed()))
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+        
+        //Print out the map after sorting
+        System.out.println("\nAfter sorting by frequence:");
+        for (Map.Entry<String, Integer> entry : sortedByFrequences.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            System.out.println("tag: " + key + " - frequences: " + value);
+        }
+        
+        //Write result to file with format "tagName,Frequences"
+        File f = new File(fileName);
+        PrintWriter pw = new PrintWriter(f);
+        pw.println("Tag,Frequences\n");
+        for (Map.Entry<String, Integer> entry : sortedByFrequences.entrySet()) {
+                String key = entry.getKey();
+                Integer value = entry.getValue();
+                pw.println(key + "," + value); 
+        }
+        
+        pw.close();
     }
 }
