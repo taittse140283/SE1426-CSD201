c43eaea885a4e2f16d66671ea0fcc950e2b44bde
Thai Tieu Minh <tieuminh2510@gmail.com>
2020-06-06 07:31:47 +0700

    CLI

---
+++ CLI/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        if(args[0].equals("-h")) {
+            System.out.print("./lab1 1 -r <<user_CSV_file>> -s <<new_user_CSV_file>>: Problem 1, read the user csv file and save the data strucutre into csv file\n");
+        }
+//        for(int i = 0 ; i < args.length; i ++) {
+//            System.out.print(args[i] + "\n");
+//        }
+
+    }
+}
