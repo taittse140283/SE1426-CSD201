89e1d31cf04ab7e285335b7fb301fc535265d382
Hoàng Gia Thiên Phúc <thienphuccoder@gmail.com>
2020-06-07 11:10:48 +0700

    Problem 2: Create ArrayStack

---
+++ LAB1/src/main/java/bai2/ArrayStack.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai2;
+
+/**
+ *
+ * @author Admin
+ */
+public class ArrayStack<E> implements Stack<E>{
+
+    private E[] list;
+    
+    @Override
+    public void push(E info) {
+        
+    }
+    @Override
+    public E pop() {
+        return null;
+    }
+    
+
+    @Override
+    public E top() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return true;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void print() {
+    }
+    
+}
