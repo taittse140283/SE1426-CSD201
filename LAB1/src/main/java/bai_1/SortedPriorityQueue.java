76fe3dbf0201fdc78b301970d1acac68f75d2e06
Linh <Linh@SE141095.mshome.net>
2020-06-07 15:19:23 +0700

    Create SortedPriorityQueue class

---
+++ LAB1/src/main/java/bai_1/SortedPriorityQueue.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai_1;
+
+/**
+ *
+ * @author Linh
+ */
+public class SortedPriorityQueue {
+    Node head, tail;
+
+    public SortedPriorityQueue() {
+        head = tail = null;
+    }
+    
+    //Using method addLast of DoublyLinkedList
+    public void addLast(String email, int point) {
+        Player p = new Player(email, point); //Create a new player
+        Node newNode = new Node(p, head, tail); //Create new node stores player info
+        
+        //This code will execute if nothing in queue
+        if(head == null) {
+            head = newNode;
+            tail = newNode;
+            head.setPrev(null);
+            tail.setNext(null);
+            return;
+        }
+        //Oppositely, this code will add new node at the of last queue
+        else {
+            newNode.setPrev(tail);
+            tail.setNext(newNode);
+            tail = newNode;
+            tail.setNext(null);
+        }
+    }
+    
+   
+}
