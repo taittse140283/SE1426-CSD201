a2433207e04a90c0360cc6eb334dc3480b58c322
Linh <Linh@SE141095.mshome.net>
2020-06-07 16:38:17 +0700

    Update add method

--- LAB1/src/main/java/bai_1/SortedPriorityQueue.java
+++ LAB1/src/main/java/bai_1/SortedPriorityQueue.java
@@ -16,8 +16,8 @@ public class SortedPriorityQueue {
         head = tail = null;
     }
     
-    //Using method addLast of DoublyLinkedList
-    public void addLast(String email, int point) {
+    //Add new node into the queue and sort it
+    public void addAndSort(String email, int point) {
         Player p = new Player(email, point); //Create a new player
         Node newNode = new Node(p, head, tail); //Create new node stores player info
         
@@ -29,13 +29,59 @@ public class SortedPriorityQueue {
             tail.setNext(null);
             return;
         }
-        //Oppositely, this code will add new node at the of last queue
-        else {
-            newNode.setPrev(tail);
-            tail.setNext(newNode);
-            tail = newNode;
-            tail.setNext(null);
+        
+        //I use the point of player as rank of them
+        //Based on their rank, i can sort the position of them in the queue
+        int rank = newNode.getInfo().getPoint();
+        
+        //if the rank of newest player higher than the biggest point
+        //I add new node to the first of queue
+        if(rank > getMaxPoint()) {
+            newNode.setNext(head);
+            head.setPrev(newNode);
+            head = newNode;
+            head.setPrev(null);
         }
+        else {
+            //I use loop to find the position for new node player
+            for(Node n  = tail; n != null; n = n.getPrev()) {
+                
+                //If the newest point is smaller or equal than the smallest point
+                //I add new node to the last of queue
+                if(rank <= getMinPoint()) {
+                    tail.setNext(newNode);
+                    newNode.setPrev(tail);
+                    tail = newNode;
+                    tail.setNext(null);
+                    return;
+                }
+                
+                //In other case, I add new node into one suitable position
+                //Behind the node has higher point and in front of the node has smaller point
+                else if(rank <= n.getInfo().getPoint()) {
+                    Node nodeAfter = n.getNext();
+                    nodeAfter.setPrev(newNode);
+                    newNode.setNext(nodeAfter);
+                    newNode.setPrev(n);
+                    n.setNext(newNode);
+                    return;
+                }
+            }
+        }   
+    }
+    
+    /**
+     * @return the biggest point
+     */
+    public int getMaxPoint() {
+        return head.getInfo().getPoint();
+    }
+    
+    /**
+     * @return the smallest point
+     */
+    public int getMinPoint() {
+        return  tail.getInfo().getPoint();
     }
     
     //Print to the screen for testing
