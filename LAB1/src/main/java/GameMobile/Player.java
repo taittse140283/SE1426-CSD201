ec5ca3c13180c75729e25cf63e34908aaef1e830
Loi Lam <Loi Lam@192.168.56.1>
2020-06-08 22:20:01 +0700

    create constructor, setter and getter

--- LAB1/src/main/java/GameMobile/Player.java
+++ LAB1/src/main/java/GameMobile/Player.java
@@ -10,5 +10,35 @@ package GameMobile;
  * @author Loi Lam
  */
 public class Player {
+
+    private String name;
+    private int point;
+
+    //Constructor không tham số
+    public Player() {
+    }
+//Constructor có tham số
+
+    public Player(String name, int point) {
+        this.name = name;
+        this.point = point;
+    }
+//Tạo getter và setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
     
 }
