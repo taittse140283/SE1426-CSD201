5afea7260defd4349063b9e71c052831f14a38c0
BachDuyHoang <bachduyhoang2k@gmail.com>
2020-06-08 22:23:10 +0700

    add comment to SortList

--- LAB1/src/main/java/LinkList.java
+++ LAB1/src/main/java/LinkList.java
@@ -1,3 +1,10 @@
+/*
+<h1> LinkList <h1>
+The interface class used to contain function
+@author Bach Duy Hoang
+@since 2020/6/1
+ */
+
 public interface LinkList {
     void removeMax();
     void findMax();
--- LAB1/src/main/java/SortList.java
+++ LAB1/src/main/java/SortList.java
@@ -4,21 +4,39 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
+/*
+<h1> SortList <h1>
+implements LinkList. Using double linked list and sorted priority queue
+@author Bach Duy Hoang
+@since 2020/6/1
+ */
 public class SortList implements LinkList {
     private DoubleLinkedList<Entry> dbl = new DoubleLinkedList<>();
     Scanner sc = new Scanner(System.in);
+
+    /*
+    This method used to remove node have max value
+     */
     @Override
     public void removeMax() {
         dbl.removeLast();
     }
 
+    /*
+    This method used to find max value of node in list
+     */
     @Override
     public void findMax() {
         System.out.println("Max: " + dbl.last());
     }
 
-    /*This method use to find post to insert new value
-    and sort. parameter is Object.
+    /*
+    This method use to find post to insert new value
+    and sort. if list is empty insert to first. if rank of
+    Entry is bigger than max value of list then add to last of list.
+    If rank of Entry is smaller or equal than min value of list then
+    add to first of list. Else find post to insert value and insert.
+    @param e This values wants to insert.
      */
     @Override
     public void insert(Entry e) {
@@ -44,9 +62,11 @@ public class SortList implements LinkList {
     }
 
 
-    /*This method will delete info of player's email
-    Parameter is email of player. If email is not exist,
-    method will show a msg to warning
+    /*
+    This method will delete info of player's email
+    .If email is not exist, method will show a msg
+    to warning. Else delete player.
+    @param mail Remove player base on mail.
      */
     @Override
     public void remove(String mail) {
@@ -60,9 +80,10 @@ public class SortList implements LinkList {
         System.out.println("Data is not exist");
     }
 
-    /*This method will find info of player
-    Parameter is email of player. If email is not exist,
-    method will show a msg to warning
+    /*
+    This method will find info of player. If email is not exist,
+    method will show a msg to warning. Else print mail and point of player
+    @param mail Email of player.
      */
     @Override
     public void find(String mail) {
@@ -77,9 +98,10 @@ public class SortList implements LinkList {
     }
 
 
-    /*This method will update point of player
-    Parameter is email of player. If email is not exist,
-    method will show a msg to warning
+    /*
+    This method will update point of player. If email is not exist,
+    method will show a msg to warning. Else update info of player
+    @param mail email of player.
      */
     @Override
     public void update(String mail) {
@@ -96,11 +118,17 @@ public class SortList implements LinkList {
         }
         System.out.println("Data is not exist");
     }
-
+    /*
+    Print list
+     */
     public void print() {
         dbl.print();
     }
 
+    /*
+    This method used to print value of node in list to file
+    @param file name of file that wants to write to
+     */
     public void printtoFile(String Filename) throws IOException {
         dbl.printtoFile(Filename);
     }
