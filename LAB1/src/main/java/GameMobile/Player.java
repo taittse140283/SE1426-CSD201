aac7a74d2583401ed60498f915b84c1cf2b4ff73
Loi Lam <Loi Lam@192.168.56.1>
2020-06-08 21:58:31 +0700

    cerate class player

---
+++ LAB1/src/main/java/GameMobile/Player.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GameMobile;
+
+/**
+ *
+ * @author Loi Lam
+ */
+public class Player {
+    
+}
