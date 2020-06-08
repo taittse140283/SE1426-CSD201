7b9bf5b4e974366008629977a654e43744641b03
Asus <Asus@SE140375>
2020-06-08 19:51:59 +0700

    test h

---
+++ LAB1/src/main/java/Bai1/DBLinkedList.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bai1;
+
+/**
+ *
+ * @author Asus
+ */
+public class DBLinkedList {
+    private static class Node<E>{
+        private Node<E> prev;
+        private Node<E> next;
+        private E e;
+        public Node(Node<E> prev, Node<E> next, E e) {
+            this.prev = prev;
+            this.next = next;
+            this.e = e;
+        }
+
+        public Node() {
+        }
+
+        public Node<E> getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node<E> prev) {
+            this.prev = prev;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+
+        public E getE() {
+            return e;
+        }
+
+        public void setE(E e) {
+            this.e = e;
+        }
+        
+       
+}
