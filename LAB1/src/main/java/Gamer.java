45838a39c06993c351fe0a2d752090772bc01697
tmt2504 <tmt2504@gmail.com>
2020-06-08 13:10:30 +0700

    abc

---
+++ LAB1/src/main/java/Gamer.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tri
+ */
+public class Gamer {
+    private String email;
+    private int point;
+
+    public Gamer(String email, int point) {
+        this.email = email;
+        this.point = point;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+    
+}
