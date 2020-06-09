3d07cd2b3db947ee494215b89b7f18308193a1b3
Thuan Le <Thuan Le@192.168.115.1>
2020-06-09 20:00:31 +0700

    updated

--- LAB1/src/main/java/DoublyLinkedList.java
+++ LAB1/src/main/java/DoublyLinkedList.java
@@ -8,46 +8,46 @@
  *
  * @author Thuan Le
  */
-public class DoublyLinkedList<E> {
-    private static class Node<E>{
-        private E element;
-        private Node<E> next;
-        private Node<E> prev;
+public class DoublyLinkedList<User> {
+    private static class Node<User>{
+        private User element;
+        private Node<User> next;
+        private Node<User> prev;
 
-        public Node(E element, Node<E> prev, Node<E> next) {
+        public Node(User element, Node<User> prev, Node<User> next) {
             this.element = element;
             this.prev = prev;
             this.next = next;
         }
 
-        public E getElement() {
+        public User getElement() {
             return element;
         }
 
-        public void setElement(E element) {
+        public void setElement(User element) {
             this.element = element;
         }
 
-        public Node<E> getNext() {
+        public Node<User> getNext() {
             return next;
         }
 
-        public void setNext(Node<E> next) {
+        public void setNext(Node<User> next) {
             this.next = next;
         }
 
-        public Node<E> getPrev() {
+        public Node<User> getPrev() {
             return prev;
         }
 
-        public void setPrev(Node<E> prev) {
+        public void setPrev(Node<User> prev) {
             this.prev = prev;
         }
         
     }
     
-    private Node<E> header;
-    private Node<E> trailer;
+    private Node<User> header;
+    private Node<User> trailer;
     private int size = 0;
     
     public DoublyLinkedList(){
@@ -61,43 +61,43 @@ public class DoublyLinkedList<E> {
     public boolean isEmpty(){ return size ==0;}
     
     
-    public E first(){
+    public User first(){
         if(isEmpty())   return null;
         return header.getNext().getElement();
     }
     
-    public E last(){
+    public User last(){
         if(isEmpty()) return null;
         return trailer.getNext().getElement();
     }
-    private void addBetween(E element, Node<E> predecessor, Node<E> successor){
-        Node<E> newest = new Node<>(element, predecessor, successor);
+    private void addBetween(User element, Node<User> predecessor, Node<User> successor){
+        Node<User> newest = new Node<>(element, predecessor, successor);
         predecessor.setNext(newest);
         successor.setPrev(newest);
         size++;
     }
-    public void addFirst(E element){
+    public void addFirst(User element){
         addBetween(element, header, header.getNext());
     }
     
-    public void addLast(E element){
+    public void addLast(User element){
         addBetween(element, trailer.getPrev(), trailer);
     }
-    private E remove(Node<E> node){
-        Node<E> predecessor = node.getPrev();
-        Node<E> successor = node.getNext();
+    private User remove(Node<User> node){
+        Node<User> predecessor = node.getPrev();
+        Node<User> successor = node.getNext();
         predecessor.setNext(successor);
         successor.setPrev(predecessor);
         size--;
         return node.getElement();
     }
     
-    public E removeFirst(){
+    public User removeFirst(){
         if(isEmpty()) return null;
         return remove(header.getNext());
     }
     
-    public E removeLast(){
+    public User removeLast(){
         if(isEmpty()) return null;
         return remove(trailer.getPrev());
     }
