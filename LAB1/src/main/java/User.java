b147e5e0c032338d5e00767f308579e91edb5206
Thuan Le <Thuan Le@192.168.115.1>
2020-06-09 19:53:45 +0700

    tạo user

---
+++ LAB1/src/main/java/User.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Thuan Le
+ */
+public class User {
+    private String email;
+    private int point;
+
+    public User() {
+    }
+
+    
+    public User(String email, int point) {
+        this.email = email;
+        this.point = point;
+    }
+    
+    
+}
