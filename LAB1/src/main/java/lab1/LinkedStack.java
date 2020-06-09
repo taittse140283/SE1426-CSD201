bb32c647f49e35a4704cf9d12aaeb544608bc8bb
luongse <luonghb24@gmail.com>
2020-06-09 13:45:53 +0700

    reup LinkedStack

--- LAB1/src/main/java/lab1/LinkedStack.java
+++ LAB1/src/main/java/lab1/LinkedStack.java
@@ -9,10 +9,36 @@ package lab1;
  *
  * @author Admin
  */
-public class LinkedStack implements Stack{
-    private DoubleLinkList list= new DoubleLinkList();
+public  class LinkedStack<Entry> implements Stack<Entry>{
+    private DoubleLinkList<Entry> list= new DoubleLinkList<Entry>();
+     @Override
+     public void add(Entry e){
+         list.addFirst(e);
+     };
+     @Override
+     public Entry remove(){
+         return list.removeFirst();
+     }
+     @Override
+     public int size(){
+         return list.size();
+     }
+     @Override
+     public Entry top(){
+        return list.first();
+     }
+     @Override
+     public boolean isEmpty(){
+         return list.isEmpty();
+         
+     }
+     @Override
+     public String toString(){
+        return  list.toString();
+         
+     }
+
     
-    
-    }
+
     
 }
