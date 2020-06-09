4d361230959ffe68af3b130c40f0667752ca2a0d
lehie <lehie@DESKTOP-QS1EETP>
2020-06-10 02:02:23 +0700

    update Player

---
+++ LAB1/src/main/java/Player.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lehie
+ */
+public class Player {
+    private String email;
+    private int point;
+    
+    public Player(){
+    }
+    
+    public Player(String email, int point){
+        this.email=email;
+        this.point=point;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+    
+    
+}
