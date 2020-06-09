2deda1a2eef8919f41ad4cc60180b1b0cbb44a57
Hong Son <hongson2992000@gmail.com>
2020-06-09 19:07:16 +0700

    Create getter and setter

--- LAB1/src/main/java/DoubleLinkedList.java
+++ LAB1/src/main/java/DoubleLinkedList.java
@@ -14,7 +14,38 @@ public class DoubleLinkedList<Gamer> {
         private Gamer data;
         private Node start;
         private Node end;
-       
+        //Create contructors 
+
+        public Node(Gamer data, Node start, Node end) {
+            this.data = data;
+            this.start = start;
+            this.end = end;
+        }
+        // Create getter and setter
+        public Gamer getData() {
+            return data;
+        }
+
+        public void setData(Gamer data) {
+            this.data = data;
+        }
+
+        public Node getStart() {
+            return start;
+        }
+
+        public void setStart(Node start) {
+            this.start = start;
+        }
+
+        public Node getEnd() {
+            return end;
+        }
+
+        public void setEnd(Node end) {
+            this.end = end;
+        }
+        
         
         
     }
