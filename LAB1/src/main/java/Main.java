4508a6d77af7f4f7b8ea0a86357044df6380b01d
Admin <Admin@192.168.5.1>
2020-06-09 16:08:09 +0700

    Check check

--- LAB1/src/main/java/Main.java
+++ LAB1/src/main/java/Main.java
@@ -16,6 +16,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Hello World");
         System.out.println("yttuyyiy");
+        System.out.println("This issssss theeeeee lasssssssst");
         // TODO code application logic here
     }
     
