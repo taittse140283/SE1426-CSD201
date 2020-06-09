127cbf6b23bd01cd733a83da7cf969d73a22acb9
Asus <Asus@SE140375>
2020-06-09 13:34:54 +0700

    test h

--- LAB1/src/main/java/Bai1/Main.java
+++ LAB1/src/main/java/Bai1/Main.java
@@ -11,9 +11,21 @@ package Bai1;
  */
 public class Main {
     public static void main(String[] args) {
-        try {
-            
-        } catch (Exception e) {
+        try{
+            if(args[0].equals("-h")){
+                System.out.println("Help: ");
+                System.out.println("./lab1 1 -r <<User_CSV_file>> -s <<new_user_CSV_file>>: Problem 1, read the user csv file and save the data structure into csv file");
+                System.out.println("./lab1 1 -r <<user_CSV_file>> -s <<new_user_CSV_file>> -a <<email>> <<point>>: Problem 1, add a new user into the data strucutre and save to new csv file");
+                System.out.println("./lab1 1 -r <<user_CSV_file>> -s <<new_user_CSV_file>> -d <<email>>: Problem 1, delete a user in the data strucutre and save to new csv file");
+                System.out.println("./lab1 1 -r <<user_CSV_file>> -s <<new_user_CSV_file>> -u <<email>> <<new_point>>: Problem 1, update new point for user in the data strucutre and save to new csv file");
+                System.out.println("./lab1 1 -r <<user_CSV_file>> -s <<new_user_CSV_file>> -dt: Problem 1, delete the top user from the data strucutre and save to new csv file");
+                System.out.println("./lab1 1 -r <<user_CSV_file>> -g <<email>>: Problem 1, get the point of user from the data strucutre");
+                System.out.println("./lab1 1 -r <<user_CSV_file>> -t: Problem 1, get the point of the top user from the data strucutre");
+            }
         }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
   
