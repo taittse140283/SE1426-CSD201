c974856700b0c4454a7a9dbbf7ae575aead38095
Administrator <Administrator@PLAYZONE-181>
2020-06-09 21:16:07 +0700

    Nested Node class

--- LAB1/src/main/java/collections/DoubleLinkedList.java
+++ LAB1/src/main/java/collections/DoubleLinkedList.java
@@ -9,6 +9,22 @@ package collections;
  *
  * @author SE140834
  */
-public class DoubleLinkedList {
-    
+public class DoubleLinkedList<E> {
+    /*Nested Node class*/
+    private static class Node<E> {
+        private E element;
+        private Node<E> prev;
+        private Node<E> next;
+        public Node(E e, Node<E> p, Node<E> n) {
+            element = e;
+            prev = p;
+            next = n;
+        }
+        public E getElement() { return element; }
+        public Node<E> getPrev() { return prev; }
+        public Node<E> getNext() { return next; }
+        public void setPrev(Node<E> p) { prev = p; }
+        public void setNext(Node<E> n) { next = n; }
+        public void setElement(E e) { element = e; }
+    }
 }
