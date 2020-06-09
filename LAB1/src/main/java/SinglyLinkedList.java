868dd2a6b68b334af49ddbaa092fb7491e97be78
lehie <lehie@DESKTOP-QS1EETP>
2020-06-09 16:37:37 +0700

    update SinglyLinkedList

---
+++ LAB1/src/main/java/SinglyLinkedList.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lehie
+ */
+public class SinglyLinkedList<E> {
+    //*This method is used to 
+    private class Node {
+        private E value;
+        private Node next;
+        
+        public Node (E value, Node node) {
+            this.value = value;
+            this.next = node;
+        }
+        
+        
+    }
+}
