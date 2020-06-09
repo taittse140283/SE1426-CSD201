407826325046a73cca4d3dde2a95fb9a38812ff3
ealflm <ealflm@192.168.58.1>
2020-06-09 15:51:27 +0700

    Update Main <3

--- LAB1/src/main/java/Main.java
+++ LAB1/src/main/java/Main.java
@@ -16,6 +16,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Hello World");
         System.out.print("Helllo Boizzzzz!");
+        System.out.print("Helloo :3");
         // TODO code application logic here
     }
     
