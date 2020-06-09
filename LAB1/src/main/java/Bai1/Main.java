e090c5611f741a68b248edf2e5779b7c227d8638
Asus <Asus@SE140375>
2020-06-09 13:31:44 +0700

    test h

---
+++ LAB1/src/main/java/Bai1/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bai1;
+
+/**
+ *
+ * @author Asus
+ */
+public class Main {
+    public static void main(String[] args) {
+        try {
+            
+        } catch (Exception e) {
+        }
+}
+  
