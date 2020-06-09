039f709a804121480e296773116c77f0a769077f
User <User@192.168.52.1>
2020-06-09 10:07:32 +0700

    chay ham main

--- LAB1/src/main/java/Bai2/Main.java
+++ LAB1/src/main/java/Bai2/Main.java
@@ -12,11 +12,12 @@ import java.util.HashMap;
  * @author User
  */
 public class Main {
+
     public static void main(String[] args) {
         HashMap<String, Integer> hashMap = new HashMap<>();
         String url = "https://vi.wikipedia.org/wiki/Trang_Ch%C3%ADnh";
-        String output = "xuat.csv";
-        
+        String output = "output.csv";
+
         Manager.process(hashMap, Manager.readHtlmFromWeb(url));
         Manager.writeToFile(hashMap, output);
     }
