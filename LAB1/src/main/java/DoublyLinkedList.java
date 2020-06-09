94f2a3db2f033e2ed0e01a18543cdb7b5a33376e
Thuan Le <Thuan Le@192.168.115.1>
2020-06-09 08:08:30 +0700

    doublylinkedlist

---
+++ LAB1/src/main/java/DoublyLinkedList.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Thuan Le
+ */
+public class DoublyLinkedList<E> {
+    private static class Node<E>{
+        private E element;
+        private Node<E> next;
+        private Node<E> prev;
+
+        public Node(E element, Node<E> prev, Node<E> next) {
+            this.element = element;
+            this.prev = prev;
+            this.next = next;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public void setElement(E element) {
+            this.element = element;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+
+        public Node<E> getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node<E> prev) {
+            this.prev = prev;
+        }
+        
+    }
+}
