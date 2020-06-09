7f2f44eecbe5a4467d2263f96e58679e809e55cd
User <User@192.168.52.1>
2020-06-09 18:00:21 +0700

    Tạo class người chơi

---
+++ LAB1/src/main/java/Bai1/Player.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bai1;
+
+/**
+ *
+ * @author User
+ */
+public class Player {
+    
+    private String email;
+    private long point;
+
+    public Player() {
+    }
+
+    public Player(String email, long point) {
+        this.email = email;
+        this.point = point;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPoint() {
+        return point;
+    }
+
+    public void setPoint(long point) {
+        this.point = point;
+    }
+
+    @Override
+    public String toString() {
+        return this.getEmail() + "," + this.getPoint();
+    }
+    
+    
+}
