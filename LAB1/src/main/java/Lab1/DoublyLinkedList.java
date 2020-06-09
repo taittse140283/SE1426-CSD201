4d70d2cc9a95c9693f7ade33993d0927c97826a1
Admin <Admin@192.168.154.1>
2020-06-09 08:39:39 +0700

    update player (), DoublyLinkedList() {}

--- LAB1/src/main/java/Lab1/DoublyLinkedList.java
+++ LAB1/src/main/java/Lab1/DoublyLinkedList.java
@@ -12,46 +12,11 @@ package Lab1;
 public class DoublyLinkedList {
     Node head;
     Node tail;
-    /*Double Linked List Node*/
-    class Node{
-        Player data;
-        Node next;
-        Node previous;
-        /* Constructor để tạo node */
-        public Node(Player data, Node next, Node previous) {
-            this.data = data;
-            this.next = next;
-            this.previous = previous;
-        }
 
-        public Player getData() {
-            return data;
-        }
-
-        public void setData(Player data) {
-            this.data = data;
-        }
-
-        public Node getNext() {
-            return next;
-        }
-
-        public void setNext(Node next) {
-            this.next = next;
-        }
-
-        public Node getPrevious() {
-            return previous;
-        }
-
-        public void setPrevious(Node previous) {
-            this.previous = previous;
-        }
-        
-        
-    }
     public DoublyLinkedList(){
-        head = new Node(null, null, null);
-        tail = new Node(null, null, head);
+        head = tail = null;
+    }
+    public boolean isEmpty(){
+        return head == null;
     }
 }
