2f7e6d6fa694db709b8d9f2f28f54559984dcd65
User <User@192.168.52.1>
2020-06-09 07:16:47 +0700

    Chú thích chức năng của từng method

--- LAB1/src/main/java/Bai2/Stack.java
+++ LAB1/src/main/java/Bai2/Stack.java
@@ -10,10 +10,39 @@ package Bai2;
  * @author User
  */
 public interface Stack<E> {
-    
+
+    /**
+     * Return the number of elements in the stack.
+     *
+     * @return number of elements in the stack.
+     */
     public int size();
+
+    /**
+     * Return whether the stack is empty.
+     *
+     * @return true if the stack is empty, false otherwise.
+     */
     public boolean isEmpty();
+
+    /**
+     * Inspect the element at the top of the stack.
+     *
+     * @return top element in the stack.
+     */
     public E top();
+
+    /**
+     * Insert an element at the top of the stack.
+     *
+     * @param element to be inserted.
+     */
     public void push(E element);
+
+    /**
+     * Remove the top element from the stack.
+     *
+     * @return element removed.
+     */
     public E pop();
 }
