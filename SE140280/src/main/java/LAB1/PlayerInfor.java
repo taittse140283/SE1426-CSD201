5ef9ca5acec7ad6b6e9571bc17eba45903283aaa
ZunPeter <ZunPeter@DESKTOP-D9HH4VK>
2020-06-09 22:47:57 +0700

    Output PlayerInformation

---
+++ SE140280/src/main/java/LAB1/PlayerInfor.java
@@ -0,0 +1,38 @@
+package LAB1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ZunPeter
+ */
+public class PlayerInfor {
+    private String email;
+    private int point;
+
+    public PlayerInfor(String email, int point) {
+        this.email = email;
+        this.point = point;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+    
+}
