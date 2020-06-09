c3cd08e95d5e07f0372052f234466336ee66f68a
Dell <Dell@SE140363>
2020-06-09 13:06:26 +0700

    Creat class ArrayStack

---
+++ LAB1/src/main/java/bai_2/ArrayStack.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai_2;
+
+/**
+ *
+ * @author Dell
+ */
+public class ArrayStack<E> implements Stack<E>{
+    public static final int CAPACITY=1000; // default array capacity 
+    private E[] data; // used to store tags
+    private int t=-1;
+
+    /**
+     * This method is constructor with default capacity
+     * No input data
+     * No output data
+     */
+    public ArrayStack() {
+        this(CAPACITY);
+    }
+    
+    /**
+     * This method is constructor with given capacity
+     * Input data is capacity
+     */
+    public ArrayStack(int capacity){
+        data=(E[]) new Object[capacity];
+    }
+    
+    /**
+     * This method to increase size of the element in stack
+     * No input data
+     * Output data : the number of element in the stack
+     */
+    @Override
+    public int size() {
+        return (t+1);
+    }
+
+    /**
+     * This method to check the stack is empty or not
+     * No input data
+     * Output data: true or false when t equal to -1
+     */
+    @Override
+    public boolean isEmpty() {
+        return (t==-1);
+    }
+
+    
+}
