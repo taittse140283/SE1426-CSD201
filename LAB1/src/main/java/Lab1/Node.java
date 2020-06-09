9e1869f6caa96b6ad29e945b9da8d276207b65b9
Admin <Admin@192.168.154.1>
2020-06-09 18:25:44 +0700

    Delete getter setter

--- LAB1/src/main/java/Lab1/Node.java
+++ LAB1/src/main/java/Lab1/Node.java
@@ -34,29 +34,5 @@ public class Node<Player> {
         this.next = next;
         this.previous = previous;
     }
-
-    public Player getData() {
-        return data;
-    }
-
-    public void setData(Player data) {
-        this.data = data;
-    }
-
-    public Node getNext() {
-        return next;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
-    }
-
-    public Node getPrevious() {
-        return previous;
-    }
-
-    public void setPrevious(Node previous) {
-        this.previous = previous;
-    }
     
 }
