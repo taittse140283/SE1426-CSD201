dcc41dd5cd8f5fa723325ff15a7b556031c13ac6
Admin <Admin@192.168.154.1>
2020-06-09 08:39:59 +0700

    Node ()

---
+++ LAB1/src/main/java/Lab1/Node.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab1;
+
+/**
+ *
+ * @author Admin
+ */
+public class Node {
+    Player data;
+    Node next;
+    Node previous;
+
+    public Node(Player data, Node next, Node previous) {
+        this.data = data;
+        this.next = next;
+        this.previous = previous;
+    }
+
+    public Player getData() {
+        return data;
+    }
+
+    public void setData(Player data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(Node previous) {
+        this.previous = previous;
+    }
+    
+}
