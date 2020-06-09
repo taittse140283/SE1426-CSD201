55843cc6931b24ef1c530b4cb6453faba7798ef3
User <User@192.168.52.1>
2020-06-09 09:28:07 +0700

    Chay chuong trinh

---
+++ LAB1/src/main/java/Bai2/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bai2;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author User
+ */
+public class Main {
+    public static void main(String[] args) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        String url = "https://vi.wikipedia.org/wiki/Trang_Ch%C3%ADnh";
+        String output = "output.csv";
+        
+        Manager.process(hashMap, Manager.readHtlmFromWeb(url));
+        Manager.writeToFile(hashMap, output);
+    }
+}
