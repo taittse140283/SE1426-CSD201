4f8b09d4a0d3460eda6c7a03ef517af34a95ed66
User <User@192.168.52.1>
2020-06-09 09:20:05 +0700

    Xử lí các thẻ html

--- LAB1/src/main/java/Bai2/Manager.java
+++ LAB1/src/main/java/Bai2/Manager.java
@@ -122,4 +122,48 @@ public class Manager {
             }
         }
     }
+
+    /**
+     * Process HTML tag Save some openingTag into arrayStack Process some
+     * special tag Element in top of stack will be compare with closingTag first
+     *
+     * @param hashMap
+     * @param html
+     */
+    public static void process(HashMap<String, Integer> hashMap, String html) {
+        ArrayStack<String> listTag = new ArrayStack<>();
+        int count;
+        int i = html.indexOf('<'); // lay vi tri xuat hien dau tien cua ki tu '<'
+        while (i != -1) { // indexOf se tra ve gia tri -1 neu khong tim thay gia tri
+            int k = html.indexOf('>', i + 1); // lay vi tri xuat hien dau cua ki tu '>'
+            String tagName = html.substring(i + 1, k); // tao chuoi con
+
+            if (tagName.contains(" ")) { // the tag chua gia tri khoang trang ex: < p class="">
+                String[] s = tagName.split(" "); // tach chuoi thanh mang ki tu bang dau phan cach la khoang trang
+                tagName = s[0]; // lay gia tri dau tien trong mang tim dc
+            }
+
+            // luu thang vao danh sach cac the dac biet
+            if (tagName.equalsIgnoreCase("area") || tagName.equalsIgnoreCase("base") || tagName.equalsIgnoreCase("br")
+                    || tagName.equalsIgnoreCase("col") || tagName.equalsIgnoreCase("!DOCTYPE") || tagName.equalsIgnoreCase("command")
+                    || tagName.equalsIgnoreCase("embed") || tagName.equalsIgnoreCase("hr") || tagName.equalsIgnoreCase("img")
+                    || tagName.equalsIgnoreCase("input") || tagName.equalsIgnoreCase("keygen") || tagName.equalsIgnoreCase("source")
+                    || tagName.equalsIgnoreCase("link") || tagName.equalsIgnoreCase("meta") || tagName.equalsIgnoreCase("param")
+                    || tagName.equalsIgnoreCase("track") || tagName.equalsIgnoreCase("wbr")) {
+                count = hashMap.containsKey(tagName) ? hashMap.get(tagName) : 0;
+                hashMap.put(tagName, count + 1);
+            } else {
+                if (!tagName.startsWith("/") && !tagName.startsWith("!")) { // neu the khong bat dau bang '/' va '!' thi luu vao danh sach va dem
+                    listTag.push(tagName);
+                    count = hashMap.containsKey(tagName) ? hashMap.get(tagName) : 0;
+                    hashMap.put(tagName, count + 1);
+                } else {
+                    if (listTag.top().equalsIgnoreCase(tagName)) {
+                        listTag.pop();
+                    }
+                }
+            }
+            i = html.indexOf('<', k + 1);
+        }
+    }
 }
