64b80351bc289a9afd98d73f0b8d864eb2039941
luongse <luonghb24@gmail.com>
2020-06-09 12:57:40 +0700

    update method delete

--- LAB1/src/main/java/lab1/DoubleLinkList.java
+++ LAB1/src/main/java/lab1/DoubleLinkList.java
@@ -9,28 +9,30 @@ package lab1;
  *
  * @author Admin
  */
-public class DoubleLinkList {
+public class DoubleLinkList<Entry> {
 
    
-    private class Node<>{
-        private int point;
-        private Node next;
-        private Node back;
-        public Node(int point, Node next_Node, Node back_Node){
-            this.point= point;
+    private class Node<Entry>{
+        private Entry element;
+        private Node<Entry> next;
+        private Node<Entry> back;
+        public Node(Entry element, Node next_Node, Node back_Node){
+            element= element;
             this.next=next_Node;
             this.back= back_Node;
         }
+        
     
     
     //getters and setters
-    public Node<> getBack(){ return back;}
-    public Node<> getNext(){ return next;}
-    public void setBack(Node <> b){back=b;}
-    public void setNext(Node <> n){next=n;}
+    public Entry getElement(){ return element;}
+    public Node<Entry> getBack(){ return back;}
+    public Node<Entry> getNext(){ return next;}
+    public void setBack(Node<Entry>  b){back=b;}
+    public void setNext(Node<Entry>  n){next=n;}
     }
-    private Node header;
-    private Node tailer;
+    private Node<Entry> header;
+    private Node<Entry> tailer;
     private int size=0;
     
     //constructor n new empty list
@@ -41,21 +43,40 @@ public class DoubleLinkList {
     }
     /** Return the number of elements in the linked list. */
     public int size(){ return size;}
-    /** tests whether the linkded list is empty */
+    /** tests whether the linked list is empty */
     public boolean isEmpty(){return size==0;}
     /** Returns the first element of the list */
-    public Node first(){
+    public Entry first(){
         if(isEmpty()) return null;
-        return header.getNext().getPoint();
+        return header.getNext().getElement();
     }
-    private void addBetween(int point, Node infontof, Node back){
+    //** add element to the linked list int the between the given nodes  */
+    private void addBetween(Entry Element, Node<Entry> infontof, Node<Entry> back){
         /** Create and link a node */
-        Node new_Node= new Node(point, infontof, back);
+        Node<Entry> new_Node= new Node<>(Element, infontof, back);
         infontof.setNext(new_Node);
         back.setBack(new_Node);
         size++;
         
+    } 
+    /** 
+    * this method remove a node in linkedList
+    * @param node
+    * no output
+    
+    */
+    private Entry remove(Node <Entry> node){
+        Node<Entry> infontof= node.getBack();
+        Node<Entry> back=node.getNext();
+        infontof.setNext(back);
+        back.setBack(infontof);
+        size--;
+        return node.getElement();
     }
+    
+    
+
+    
     
     
 }
