be2893f0c7b9af02b1df358a6e0ba1767283cff1
Thai Tieu Minh <tieuminh2510@gmail.com>
2020-06-10 15:10:15 +0700

    delete in tree

--- LOC34/src/com/company/BSTree.java
+++ LOC34/src/com/company/BSTree.java
@@ -4,9 +4,14 @@ public class BSTree {
     private class Node {
         private int info;
         private Node left, right;
-        public  Node(int info) {
+        public Node(int info) {
             this.info = info;
             left = right =  null;
+        }
+        public String toString() {
+            String result = "";
+            result += info;
+            return result;
         }
     }
     Node root;
@@ -26,6 +31,42 @@ public class BSTree {
             p.left = insertNode(value, p.left);
         }
         return p;
+    }
+    private Node search(int value,  Node tree) {
+        if(tree == null) return null;
+        if(value < tree.info ) return search(value, tree.left);
+        else if (value > tree.info ) return search(value, tree.right);
+        else return tree;
+    }
+
+    private Node parent(Node n, Node tree) {
+        if(tree == null) return null;
+        if(n == tree.left || n == tree.right) return tree;
+        else if(n.info > tree.info) return parent(n, tree.right);
+        else if(n.info < tree.info) return parent(n, tree.left);
+        else return null;
+    }
+    private Node right(Node tree) {
+        if(tree.right == null) return tree;
+        else return right(tree.right);
+    }
+    public void delete(int value) {
+        Node n = search(value, root);
+        Node p = parent(n, root);
+        if(n.left == n.right && n.right == null) {
+            if(p.right == n) p.right = null;
+            else p.left = null;
+        } else if(n.left == null || n.right == null) {
+            if(n.right != null)
+                if(p.right == n) p.right = n.right;
+                else p.left = n.right;
+            else
+                if(p.right == n) p.right = n.left;
+                else p.left = n.left;
+        } else {
+            Node r = right(n.left);
+        }
+
     }
     void inOrder(Node p) {
         if (p == null) return;
--- LOC34/src/com/company/Main.java
+++ LOC34/src/com/company/Main.java
@@ -3,11 +3,27 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         BSTree tree = new BSTree();
-        tree.insert(6);
+        tree.insert(4);
         tree.insert(7);
         tree.insert(2);
         tree.insert(9);
-        tree.insert(2);
+        tree.insert(8);
+        tree.insert(6);
+        tree.insert(5);
+        tree.delete(7);
         tree.print();
+
+//        Heap h = new Heap(1000);
+//        h.insert(5);
+//        h.insert(7);
+//        h.insert(2);
+//        h.insert(9);
+//        h.insert(3);
+//        h.insert(1);
+//        h.insert(10);
+//        h.remove();
+//        h.print();
+//        h.remove();
+//        h.print();
     }
 }
