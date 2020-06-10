56114f5c842f6e603b91727c8bab20138b325f17
User <User@192.168.52.1>
2020-06-10 21:08:35 +0700

    chay thu

---
+++ LAB1/src/main/java/Bai1/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bai1;
+
+/**
+ *
+ * @author User
+ */
+public class Main {
+    public static void main(String[] args) {
+        // test cai ham da lam
+        PriorytiQueue queue = IOFile.readFromFile("user.csv");
+        IOFile.writeToFile(queue, "outputPlayer.csv");
+    }
+}
