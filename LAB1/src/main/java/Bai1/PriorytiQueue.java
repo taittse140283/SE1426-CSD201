a98fa05dec8bab656631aa499aa30098da47513e
User <User@192.168.52.1>
2020-06-10 20:21:12 +0700

    summary content of priorityQueue

--- LAB1/src/main/java/Bai1/PriorytiQueue.java
+++ LAB1/src/main/java/Bai1/PriorytiQueue.java
@@ -9,6 +9,16 @@ package Bai1;
  *
  * @author User
  */
+
+/*
+  * Its class will be get list of doublyLinkedList to process
+  * Create class priorityQueue contains method:
+   - addAndSort: Add new player into list and sort its point
+   - update: Edit information of player
+   - removePlayerHasMaxPoint with O(1)
+   - getPlayerHasMaxPoint with O(1)
+   - delete any player with specific email of player
+*/
 public class PriorytiQueue {
     
     
