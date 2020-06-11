383ea9b80871c3b4a4ab629e533d133218f65232
Admin <Admin@192.168.154.1>
2020-06-11 22:22:02 +0700

    Priority Queue

---
+++ LAB1/src/main/java/Lab1/PriorityQueue.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab1;
+
+/**
+ *
+ * @author Admin
+ */
+public class PriorityQueue{
+    DoublyLinkedList list = new  DoublyLinkedList();
+
+    public PriorityQueue() {
+    }
+    
+    public void add(Player data){
+        add(data);
+    }
+    
+    public void delete(Player data){
+        delete(data);
+    }
+    
+    public void update(Player data){
+        
+    }
+    
+    public void search(String email){
+        search(email);
+    }
+    
+    public void getTop(){
+        
+    }
+    
+    public void removeTop(){
+        
+    }
+    
+}
