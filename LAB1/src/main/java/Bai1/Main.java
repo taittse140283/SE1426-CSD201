461d64f7053acc166480ac2064b175c68d2ff6c5
User <User@192.168.52.1>
2020-06-12 20:29:08 +0700

    dong ham main

--- LAB1/src/main/java/Bai1/Main.java
+++ LAB1/src/main/java/Bai1/Main.java
@@ -3,16 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Bai1;
-
-/**
- *
- * @author User
- */
-public class Main {
-    public static void main(String[] args) {
-        // test cai ham da lam
-        PriorytiQueue queue = IOFile.readFromFile("user.csv");
-        IOFile.writeToFile(queue, "outputPlayer.csv");
-    }
-}
+//package Bai1;
+//
+///**
+// *
+// * @author User
+// */
+//public class Main {
+//    public static void main(String[] args) {
+//        // test cai ham da lam
+//        PriorytiQueue queue = IOFile.readFromFile("user.csv");
+//        IOFile.writeToFile(queue, "outputPlayer.csv");
+//    }
+//}
