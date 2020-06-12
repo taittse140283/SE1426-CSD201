0fa183abf61fce614ad017064bec6e964c6851e3
User <User@192.168.52.1>
2020-06-12 21:05:22 +0700

    dong ham main bai 2

--- LAB1/src/main/java/Bai2/Main.java
+++ LAB1/src/main/java/Bai2/Main.java
@@ -3,23 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Bai2;
-
-import java.util.HashMap;
-
-/**
- *
- * @author User
- */
-public class Main {
-
-    public static void main(String[] args) {
-        HashMap<String, Integer> hashMap = new HashMap<>();
-        String url = "https://vi.wikipedia.org/wiki/Trang_Ch%C3%ADnh";
-        String output = "output1.csv";
-
-            // test
-        Manager.process(hashMap, Manager.readHtlmFromWeb(url));
-        Manager.writeToFile(hashMap, output);
-    }
-}
+//package Bai2;
+//
+//import java.util.HashMap;
+//
+///**
+// *
+// * @author User
+// */
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        HashMap<String, Integer> hashMap = new HashMap<>();
+//        String url = "https://vi.wikipedia.org/wiki/Trang_Ch%C3%ADnh";
+//        String output = "output8.csv";
+//
+//            // test
+//        Manager.process(hashMap, Manager.readHtlmFromWeb(url));
+//        Manager.writeToFile(hashMap, output);
+//    }
+//}
