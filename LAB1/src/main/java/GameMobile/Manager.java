fc7dc0cbef670f48857f190b0196651d8abdbac0
Loi Lam <Loi Lam@192.168.56.1>
2020-06-13 15:32:53 +0700

    create class Manager for Player

---
+++ LAB1/src/main/java/GameMobile/Manager.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GameMobile;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Loi Lam
+ */
+public class Manager extends LinkedList<Player>{
+    
+}
