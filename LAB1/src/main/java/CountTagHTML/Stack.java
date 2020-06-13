96a4e9e6ac4a9dff192b187080baa1c12683aca0
tcoyl98 <62016974+tcoyl98@users.noreply.github.com>
2020-06-14 02:01:03 +0700

    create push

--- LAB1/src/main/java/CountTagHTML/Stack.java
+++ LAB1/src/main/java/CountTagHTML/Stack.java
@@ -17,4 +17,21 @@ public class Stack {
     public Stack() {
         head = tail = null;
     }
+    
+    public void push (String tag){
+        TagHTML th = new TagHTML(tag, head, tail);
+        //new node head and tail when stack null
+        if(head == null){
+            head = th;
+            head.prev = null;
+            tail = th;
+            tail.next = null;
+            return;
+        }
+        //add new node
+        th.prev = tail;
+        tail.next = th;
+        tail = th;
+        tail.next = null;
+    }
 }
