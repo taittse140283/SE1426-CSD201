bd89b1a64b85503c1688ef4a8ec9260e9bdd9109
hiep <hiep@192.168.56.1>
2020-06-13 10:51:32 +0700

    class entry

---
+++ LAB1/src/main/java/Entry.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hiep
+ */
+public class Entry {
+    private int point;
+    private String email;
+
+    public Entry(int point, String email) {
+        this.point = point;
+        this.email = email;
+    }
+
+    public Entry() {
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + email  + "," + point + '}';
+    }
+   
+}
