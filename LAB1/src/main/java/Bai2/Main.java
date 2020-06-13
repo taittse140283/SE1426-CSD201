e1d93f0ebe8010ef02e8fade89cef88a528f91c6
Asus <Asus@SE140375>
2020-06-13 13:20:58 +0700

    test h

--- LAB1/src/main/java/Bai2/Main.java
+++ LAB1/src/main/java/Bai2/Main.java
@@ -10,7 +10,7 @@ package Bai2;
  * @author Asus
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         
     }
 }
