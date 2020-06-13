fdaf8bbddf40df7d141197fd34d233a3fdb16df0
Loi Lam <Loi Lam@192.168.56.1>
2020-06-13 17:47:40 +0700

    create class Node, constructor, getter and setter

--- LAB1/src/main/java/GameMobile/Node.java
+++ LAB1/src/main/java/GameMobile/Node.java
@@ -10,5 +10,44 @@ package GameMobile;
  * @author Loi Lam
  */
 public class Node {
-    
+
+    private Player info;
+    private Node next, prev;
+
+    //Constructor no parameters
+    public Node() {
+    }
+
+    //Constructor have parameters
+    public Node(Player info, Node next, Node prev) {
+        this.info = info;
+        this.next = next;
+        this.prev = prev;
+    }
+
+    // getter and setter
+    public Player getInfo() {
+        return info;
+    }
+
+    public void setInfo(Player info) {
+        this.info = info;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
 }
