29572ed0b8bab7a9295eced75b37bcdefb9ac1cd
Hoàng Gia Thiên Phúc <thienphuccoder@gmail.com>
2020-06-13 08:40:59 +0700

    Write again parsingArguments() method of Problem 1, do not test yet

--- LAB1/src/main/java/bai1/PlayerManager.java
+++ LAB1/src/main/java/bai1/PlayerManager.java
@@ -13,7 +13,71 @@ public class PlayerManager {
     {
         String outputfileName="";
         try {
-            for(int i=0;i<args.length;i++)
+            if(args[0].equals("-r"))
+            {
+                queue=CSV.readFile(args[1]);
+                if(args[2].equals("-s"))
+                {
+                    if(args[4].equals("-a"))
+                    {
+                        try {
+                            DLLNode<Player> newest=new DLLNode<>();//create new node
+                            Player newPlayer=new Player();//create new Player
+                            newPlayer.setEmail(args[5]);//set email for Player
+                            if(Integer.parseInt(args[6])<0)
+                                throw new Exception("Error:Point must >10");//check if point <0 or not
+                            //If not, then set point for new Player
+                            newPlayer.setPoint(Integer.parseInt(args[6]));
+                            newest.setInfo(newPlayer);//set Info for newest Node
+                            queue.add(newest);//add it into Queue
+                        }catch(NumberFormatException e)
+                        {
+                            System.out.println("Error: Not compatible variable data type!");
+                        }
+                        catch (Exception e) {
+                            System.out.println(e.getMessage());
+                        }
+
+                    }
+                    else if(args[4].equals("-d"))
+                    {
+                        queue.deletePlayer(args[5]);//delete Player
+                    }
+                    else if(args[4].equals("-u"))
+                    {
+                        queue.updatePlayer(args[5],args[6]);//update Player
+                    }
+                    else if(args[4].equals("-dt"))
+                    {
+                        System.out.println("Delete top player:"+queue.removeMax());//remove Player has highest score
+                    }
+                    else
+                    {
+                        System.out.println("Error: Arguments not supported. Check helps to correct argument! Thanks!");
+                    }
+                    CSV.writeFile(args[3],queue);
+                }
+                else if(args[2].equals("-g"))
+                {
+                    if(queue.getNode(args[3])!=null) {
+                        System.out.println("Point of Player " + args[3] + "is " + queue.getNode(args[3]).getInfo().getPoint());
+                    }else
+                        System.out.println("Error: Not found player");
+                }
+                else if(args[2].equals("-t"))
+                {
+                    System.out.println("Highest point: "+queue.getMax().getPoint());
+                }
+                else
+                {
+                    System.out.println("Error: Arguments not supported. Check helps to correct argument! Thanks!");
+                }
+            }
+            else {
+                System.out.println("Error: Arguments not supported. Check helps to correct argument! Thanks!");
+            }
+
+            /*for(int i=0;i<args.length;i++)
             {
                 switch (args[i])
                 {
@@ -65,11 +129,8 @@ public class PlayerManager {
                     case "-t":
                         System.out.println("Highest point: "+queue.getMax().getPoint());
                         break;
-                    default:
-                        System.out.println("Error: Not valid arguments");
-                        break;
                 }
-            }
+            }*/
         }
         catch (IndexOutOfBoundsException e)
         {
