47b07b526a9148b6f893d2d5c6f48eaf6843b507
ZunPeter <ZunPeter@DESKTOP-D9HH4VK>
2020-06-13 10:36:55 +0700

    Stack-size-isEmpty

--- SE140280/src/main/java/LAB1_02/Stack.java
+++ SE140280/src/main/java/LAB1_02/Stack.java
@@ -10,5 +10,31 @@ package LAB1_02;
  * @author ZunPeter
  */
 public class Stack {
-    
+   NodeHTML head;
+   NodeHTML tail;
+
+    public Stack() {
+    head = null;
+    tail = null;
+    }
+    /*return the number of elements in the stack
+    @return number of element in stack*/
+    private int size = 0;
+    int size(){
+      return size;
+    }
+    /*Check if stack is Empty
+    @return true if the stack is empty */
+    public boolean isEmpty(){
+       return size == 0; 
+    }
+   
+    public void push(String tagHTML){
+        NodeHTML tag= new NodeHTML(tagHTML, head, tail);
+        
+        if(isEmpty()){
+            
+        }
+    }
+   
 }
