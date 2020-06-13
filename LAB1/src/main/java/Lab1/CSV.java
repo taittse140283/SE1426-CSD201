7151e576022d0c97addc7c875712647cb65e99a0
Admin <Admin@192.168.154.1>
2020-06-13 18:02:50 +0700

    CSV

--- LAB1/src/main/java/Lab1/CSV.java
+++ LAB1/src/main/java/Lab1/CSV.java
@@ -5,13 +5,44 @@
  */
 package Lab1;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 /**
  *
  * @author Admin
  */
 public class CSV {
     
-    public static PriorityQueue readFile (String file){
-        
+    public static PriorityQueue readFile (String file) throws IOException{
+        FileReader fr = null;
+        BufferedReader br = null;
+        PriorityQueue queue = new PriorityQueue();
+        try {
+            fr = new FileReader(file);
+            br=new BufferedReader(fr);
+            String s = br.readLine();
+            while (br.ready()) {
+                s = br.readLine();
+                StringTokenizer stk = new StringTokenizer(s, ", ");
+                String email = stk.nextToken();
+                int point = Integer.parseInt(stk.nextToken());
+                
+                Player player = new Player(email, point);
+                queue.addPlayer(player);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            if(br != null){
+                br.close();
+            }
+            if(fr !=null){
+                fr.close();
+            }
+        }
+        return queue;
     }
 }
