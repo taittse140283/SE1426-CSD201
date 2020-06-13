ba95c4f0c96a1ac3cc63f1334feb25f58ce27dad
tqhhuy <huy95huy95@gmail.com>
2020-06-14 00:29:57 +0700

    isempty, first, last

--- LAB1/src/main/java/mobilegame/DoublyLinkedList.java
+++ LAB1/src/main/java/mobilegame/DoublyLinkedList.java
@@ -10,12 +10,15 @@ package mobilegame;
  * @author Admin
  */
 public class DoublyLinkedList<E> {
+
     // khai báo class Node
-    private static class Node<E>{
+    private static class Node<E> {
+
         private E element; // tham chiếu giá trị lưu trữ trong nodde
         private Node<E> next; // tham chiếu giá trị tiếp theo
         private Node<E> prev; // tham chiếu giá trị sau đó
-        public Node (E e, Node<E> n, Node<E> p) {
+
+        public Node(E e, Node<E> n, Node<E> p) {
             this.element = e;
             this.next = n;
             this.prev = p;
@@ -40,19 +43,33 @@ public class DoublyLinkedList<E> {
         public void setPrev(Node<E> prev) {
             this.prev = prev;
         }
-        
+
     }
 
     private Node<E> header;
     private Node<E> trailer;
-    private int size=0;
-    
-    public DoublyLinkedList(){
-    header=new Node(null, null, null);
-    trailer=new Node(null, header, null);
-    header.setNext(trailer);
-    }
-  
-    
+    private int size = 0;
 
+    public DoublyLinkedList() {
+        header = new Node(null, null, null);
+        trailer = new Node(null, header, null);
+        header.setNext(trailer);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    public E first(){
+        if(isEmpty()) return null;
+        return header.getNext().getElement();
+    }
+    public E last(){
+        if(isEmpty()) return null;
+        return trailer.getPrev().getElement();
+    }
+    
 }
