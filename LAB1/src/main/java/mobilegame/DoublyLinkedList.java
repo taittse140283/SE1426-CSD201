1c5da5678945b4f67e15fcbf3a3d40894e16933c
tqhhuy <huy95huy95@gmail.com>
2020-06-14 00:42:03 +0700

    remove, removeFirst, removeLast

--- LAB1/src/main/java/mobilegame/DoublyLinkedList.java
+++ LAB1/src/main/java/mobilegame/DoublyLinkedList.java
@@ -63,25 +63,52 @@ public class DoublyLinkedList<E> {
     public boolean isEmpty() {
         return size == 0;
     }
-    public E first(){
-        if(isEmpty()) return null;
+
+    public E first() {
+        if (isEmpty()) {
+            return null;
+        }
         return header.getNext().getElement();
     }
-    public E last(){
-        if(isEmpty()) return null;
+
+    public E last() {
+        if (isEmpty()) {
+            return null;
+        }
         return trailer.getPrev().getElement();
     }
-    private void addBetween(E e, Node<E>  predecessor, Node<E> successor){
-        Node<E> newest = new Node<>(e,predecessor,successor);
+
+    private void addBetween(E e, Node<E> predecessor, Node<E> successor) {
+        Node<E> newest = new Node<>(e, predecessor, successor);
         predecessor.setNext(newest);
         successor.setPrev(newest);
         size++;
     }
-    
-    public void addFirst(E e){
-        addBetween(e,header,header.getNext());
+
+    public void addFirst(E e) {
+        addBetween(e, header, header.getNext());
     }
-    public void addLast(E e){
-        addBetween(e,trailer.getPrev(),trailer);
+
+    public void addLast(E e) {
+        addBetween(e, trailer.getPrev(), trailer);
+    }
+
+    private E remove(Node<E> node){
+        Node<E> predecessor= node.getPrev();
+        Node<E> successor=node.getNext();
+        predecessor.setNext(successor);
+        successor.setPrev(predecessor);
+        size--;
+        return node.getElement();
+    }
+    
+    public E removeFirst(){
+        if(isEmpty()) return null;
+        return remove(header.getNext());
+    }
+    
+    public E removeLast(){
+        if(isEmpty()) return null;
+        return  remove(trailer.getPrev());
     }
 }
