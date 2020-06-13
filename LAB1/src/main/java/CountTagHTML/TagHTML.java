198f8a39577a2668e50f489c8e2850c757c5f86f
tcoyl98 <62016974+tcoyl98@users.noreply.github.com>
2020-06-14 00:12:55 +0700

    create class TagHTML, constructor, getter and setter

--- LAB1/src/main/java/CountTagHTML/TagHTML.java
+++ LAB1/src/main/java/CountTagHTML/TagHTML.java
@@ -10,5 +10,44 @@ package CountTagHTML;
  * @author Loi Lam
  */
 public class TagHTML {
-    
+
+    private String tag;
+    TagHTML next, prev;
+
+    //Constructor no parameters
+    public TagHTML() {
+    }
+
+    //Constructor have parameters
+    public TagHTML(String tag, TagHTML next, TagHTML prev) {
+        this.tag = tag;
+        this.next = next;
+        this.prev = prev;
+    }
+
+    //Getter va setter
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public TagHTML getNext() {
+        return next;
+    }
+
+    public void setNext(TagHTML next) {
+        this.next = next;
+    }
+
+    public TagHTML getPrev() {
+        return prev;
+    }
+
+    public void setPrev(TagHTML prev) {
+        this.prev = prev;
+    }
+
 }
