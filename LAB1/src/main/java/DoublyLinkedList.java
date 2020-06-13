8e74542ec2721ecf72788d7c74d8e6aee2c56d09
Thuan Le <Thuan Le@192.168.115.1>
2020-06-13 14:40:28 +0700

    Ghi chu

--- LAB1/src/main/java/DoublyLinkedList.java
+++ LAB1/src/main/java/DoublyLinkedList.java
@@ -9,6 +9,13 @@
  * @author Thuan Le
  */
 public class DoublyLinkedList<User> {
+    /**
+     * Nested Node class
+     * Reference to the element stored at this node
+     * Reference to the previous node in the list
+     * Reference to the subsequent node in the list
+     * @param <User> 
+     */
     private static class Node<User>{
         private User element;
         private Node<User> next;
@@ -45,44 +52,90 @@ public class DoublyLinkedList<User> {
         }
         
     }
-    
+    /**
+     * Instance variables of the DoublyLinkedList
+     * Header sentinel
+     * Trailer sentinel
+     * Size is number of elements in the list
+     */
     private Node<User> header;
     private Node<User> trailer;
     private int size = 0;
     
+    /**
+     * Constructs a new empty list
+     * Create header
+     * Trailer is preceded by header
+     * Header is followed by trailer
+     */
     public DoublyLinkedList(){
         header = new Node<>(null, null, null);
         trailer = new Node<>(null, header, null);
         header.setNext(trailer);
     }
     
+    /**
+     * 
+     * @return the number of elements in the linked list
+     */
     public int size(){ return size;}
     
+    /**
+     * Test whether the linked list is empty
+     * @return 
+     */
     public boolean isEmpty(){ return size ==0;}
     
-    
+    /**
+     * 
+     * @return (not remove) the first element of list
+     */
     public User first(){
         if(isEmpty())   return null;
         return header.getNext().getElement();
     }
-    
+    /**
+     * 
+     * @return (not remove) the last element of list
+     */
     public User last(){
         if(isEmpty()) return null;
         return trailer.getNext().getElement();
     }
+    
+    /**
+     * Adds element to the linked list in the between the given nodes
+     * @param element
+     * @param predecessor
+     * @param successor 
+     */
     private void addBetween(User element, Node<User> predecessor, Node<User> successor){
         Node<User> newest = new Node<>(element, predecessor, successor);
         predecessor.setNext(newest);
         successor.setPrev(newest);
         size++;
     }
+    /**
+     * Add element to the front of the list
+     * @param element 
+     */
     public void addFirst(User element){
         addBetween(element, header, header.getNext());
     }
     
+    /**
+     * Add element to the end of the list
+     * @param element 
+     */
     public void addLast(User element){
         addBetween(element, trailer.getPrev(), trailer);
     }
+    
+    /**
+     * Removes the given node from the list and returns its element
+     * @param node
+     * @return 
+     */
     private User remove(Node<User> node){
         Node<User> predecessor = node.getPrev();
         Node<User> successor = node.getNext();
@@ -91,12 +144,19 @@ public class DoublyLinkedList<User> {
         size--;
         return node.getElement();
     }
-    
+    /**
+     * Removes and returns the first element of the list
+     * @return 
+     */
     public User removeFirst(){
         if(isEmpty()) return null;
         return remove(header.getNext());
     }
     
+    /**
+     * Removes and returns the last element of the list
+     * @return 
+     */
     public User removeLast(){
         if(isEmpty()) return null;
         return remove(trailer.getPrev());
