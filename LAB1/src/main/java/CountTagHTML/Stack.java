c4aa98f2f1dcd86be0e6bc028a4139a9962ae69c
tcoyl98 <62016974+tcoyl98@users.noreply.github.com>
2020-06-14 00:17:06 +0700

    create class stack and param

---
+++ LAB1/src/main/java/CountTagHTML/Stack.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CountTagHTML;
+
+/**
+ *
+ * @author Loi Lam
+ */
+public class Stack {
+
+    TagHTML head, tail;
+
+}
