504c8a5a4c35154754f945c09dbdfd7cf62ccccc
ZunPeter <ZunPeter@DESKTOP-D9HH4VK>
2020-06-13 10:15:45 +0700

    create Node class

--- SE140280/src/main/java/LAB1_02/Node.java
+++ SE140280/src/main/java/LAB1_02/Node.java
@@ -10,5 +10,39 @@ package LAB1_02;
  * @author ZunPeter
  */
 public class Node {
+    String tag;
+    Node next;
+    Node prev;
+
+    public Node(String tag, Node next, Node prev) {
+        this.tag = tag;
+        this.next = next;
+        this.prev = prev;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+    
     
 }
