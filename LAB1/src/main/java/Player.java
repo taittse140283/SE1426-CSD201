d4fa523ed1350204942b7d7aacf5e183077a7918
Admin <Admin@192.168.5.1>
2020-06-13 16:47:58 +0700

    Player

---
+++ LAB1/src/main/java/Player.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Trịnh Anh Tài
+ */
+
+/**
+  * Create class Player to save information of player. 
+  * Two properties: email and point(điểm số)
+  * Getter,Setter to support for querying data
+*/
+public class Player {
+    private String email;
+    private int point;
+    
+    public Player() {}
+    
+    public Player(String email, int point) {
+        this.email = email;
+        this.point = point;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+    
+    public String toString() {
+        return this.getEmail() + ", " + this.getPoint();
+    } 
+}
