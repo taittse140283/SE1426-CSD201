fae89961b7ce18885e02dc3c42a486e8f2ffe811
truong <truong@SE140379>
2020-06-14 16:35:37 +0700

    do doublelinklist again

---
+++ LAB1/src/main/java/CSD201_LAB1/DoubleLinkList.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CSD201_LAB1;
+
+/**
+ *
+ * @author truong
+ */
+public class DoubleLinkList {
+    public static class node{
+        node left=null;
+        node right= null;
+        private String email;
+        private int point;
+
+        public node(String email, int point) {
+            this.email = email;
+            this.point = point;
+        }
+        
+        
+    
+}
+    
+}
