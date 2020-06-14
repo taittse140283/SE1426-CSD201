130fb1df1bd50c4014c02d3fbb89d345487ed95d
hiep <hiep@192.168.56.1>
2020-06-14 11:50:09 +0700

    ham push

--- LAB1/src/main/java/Lab1_2/Stack.java
+++ LAB1/src/main/java/Lab1_2/Stack.java
@@ -10,15 +10,33 @@ package Lab1_2;
  * @author hiep
  */
 public class Stack {
-    Node head,tail;
+
+    Node head, tail;
 
     public Stack() {
-        head=null;
-        tail=null;
-    }
-    public boolean isEmpty(){
-        return tail==null;
+        head = null;
+        tail = null;
     }
 
-    
+    public boolean isEmpty() {
+        return tail == null;
+    }
+
+    public void push(String tagHTML) {
+        Node tag = new Node(head, tail, tagHTML);
+        // check list rongg
+        if (isEmpty()) {
+            head = tag;
+            tail = head;
+            tail.next = null;
+            head.prev = tail.next = null;
+            return;
+        }
+        //if not null->(addFirst)
+        tag.next = head;
+        head.prev = tag;
+        head = tag;
+        head.prev = null;
+    }
+
 }
