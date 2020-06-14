dd56348d175795e8323a7f5f64b00dff04ba427f
LiemOtis1662000 <66248873+LiemOtis1662000@users.noreply.github.com>
2020-06-14 19:16:50 +0700

    something wrong with old commit. So, push new commit

--- LAB1/src/main/java/Bai_1/readFile.java
+++ LAB1/src/main/java/Bai_1/readFile.java
@@ -18,25 +18,28 @@ import java.util.StringTokenizer;
  * @author Dell
  */
 public class readFile {
-    public static PriorytiQueue readFromFile(String file) {
+    /**
+     * This method below here is used to read each line from file
+     Input data is file
+     * @param file
+     * @return 
+     */
+    public static PriorityQueue readFromFile(String file) {
         File f= null;
         FileReader fr = null;
         BufferedReader br = null;
-        PriorytiQueue queue = new PriorytiQueue();
+        PriorityQueue queue = new PriorityQueue();
         try {
             f = new File(file);
             fr = new FileReader(file);
             br = new BufferedReader(fr);
             String s = null;
             while ((s=br.readLine())!=null) {
-                //StringTokenizer stk = new StringTokenizer(s, ", ");
-                //String email = stk.nextToken();
-                String[] stk= s.split(",");
-                String[] email=stk.nextToken();
+                StringTokenizer stk = new StringTokenizer(s, ", ");
+                String email=stk.nextToken();
                 int point = Integer.parseInt(stk.nextToken());
-
                 Infor player = new Infor(email, point);
-                queue.addAndSort(player);
+                queue.add(player);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -54,93 +57,31 @@ public class readFile {
         }
         return queue;
     }
-
-    public void writeToFile(PriorytiQueue queue, String file) {
-        if (queue.getList().getSize() == 0) {
-            System.out.println("Error: List empty");
-        } else {
-            File f = null;
-            FileWriter fw = null;
-            PrintWriter pw = null;
-            try {
-                f = new File(file);
-                fw = new FileWriter(f);
-                pw = new PrintWriter(fw);
-                pw.println("Email, point");
-                DoublyLinkedList.Node<Player> current = queue.getList().getHeader();// lay gia tri dau tien cua hang doi
-                while(current.getNext() != null) {// duyet danh sach cho den phan tu cuoi cung
-                    pw.println(current.getElement().toString());
-                    current = current.getNext();// buoc nhay
-                }
-            } catch (IOException e) {
+    /**
+     * This method below here is used to write data to file
+     * Input data is queue and file
+     * @param queue
+     * @param file
+     * @throws IOException 
+     */
+    public void writeToFile(PriorityQueue queue, String file) throws IOException {
+        File f=null;
+        FileWriter fw=null;
+        PrintWriter pw= null;
+            try{
+                f= new File(file);
+                fw= new FileWriter(f);
+                pw= new PrintWriter(fw);
+            } catch (Exception e){
                 e.printStackTrace();
-            } finally {
-                try {
-                    if(pw != null) {
-                        pw.close();
-                    }
-                    if(fw != null) {
-                        fw.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
+            } finally{
+                if(pw!=null){
+                    pw.close();
+                }
+                if(fw!=null){
+                    fw.close();
                 }
             }
-        }
-    }
 }
-public long get(String email) {
-        for(int i = 0; i < this.size(); ++i) {
-            if (((Infor)this.list.get(i)).getEmail().equals(email)) {
-                return ((Infor)this.list.get(i)).getPoint();
-            }
-        }
-
-        return -1L;
-    }
-     public void remove(String email) {
-        for(int i = 0; i < this.size(); ++i) {
-            if (((Infor)this.list.get(i)).getEmail().equals(email)) {
-                this.list.remove(i);
-            }
-        }
-
-    }
-
-    public void update(String email, int point) {
-        for(int i = 0; i < this.size(); ++i) {
-            if (((Infor)this.list.get(i)).getEmail().equals(email)) {
-                Infor info = (Infor)this.list.remove(i);
-                info.setPoint(point);
-                this.add(info);
-            }
-        }
-
-    }
-
-    public Infor removeMin() {
-        return (Infor)this.list.remove(0);
-    }
-
-    public Infor min() {
-        return (Infor)this.list.get(0);
-    }
-
-    public boolean isEmpty() {
-        return this.list.isEmpty();
-    }
-
-    public int size() {
-        return this.list.size();
-    }
-
-    public String toString() {
-        String result = "";
-
-        Infor info;
-        for(Iterator var2 = this.list.iterator(); var2.hasNext(); result = result + info.toString() + "\n") {
-            info = (Infor)var2.next();
-        }
-
-        return result;
-    }
+}
+    
