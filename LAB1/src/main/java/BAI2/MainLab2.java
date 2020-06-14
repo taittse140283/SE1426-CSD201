9484a44a06e0e4031e0a8de0917f8fdd945c64c8
Thuan Le <Thuan Le@192.168.115.1>
2020-06-14 19:03:49 +0700

    update

--- LAB1/src/main/java/BAI2/MainLab2.java
+++ LAB1/src/main/java/BAI2/MainLab2.java
@@ -10,12 +10,10 @@ package BAI2;
  * @author Thuan Le
  */
 public class MainLab2 {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) {
         String url = "https://www.facebook.com/?ref=tn_tnmn";
         String file = "outputlab2.csv";
         Manager run = new Manager();
         run.runLab2(url, file);
-        
     }
-    
 }
