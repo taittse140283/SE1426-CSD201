731067b8d306b1447cde94f940045ceea9ed9a9f
luongse <luonghb24@gmail.com>
2020-06-14 09:13:16 +0700

    add check special tag

---
+++ LAB1/src/main/java/bai2/WriteFile.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author Admin
+ */
+public class WriteFile {
+    public void WriteToFile(HashMap<String,Integer> hashmap,String fileName) throws FileNotFoundException{
+        Map<String, Integer> sort = hashmap.entrySet().stream()
+            .sorted((Map.Entry.<String, Integer> comparingByValue().reversed()))
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+        System.out.println("\nAfter sorting by value:");
+        for (Map.Entry<String, Integer> entry : sort.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            System.out.println("tag: " + key + " value: " + value);
+        }
+            File f = new File(fileName);
+        PrintWriter print = new PrintWriter(f);
+        print.println("Tag,Value\n");
+        for (Map.Entry<String, Integer> entry : sort.entrySet()) {
+                String key = entry.getKey();
+                Integer value = entry.getValue();
+                print.println(key + "," + value); 
+        }      
+        print.close();
+    }
+}
