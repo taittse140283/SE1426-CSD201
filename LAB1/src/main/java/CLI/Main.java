b5d105ff88ac7228528d353c183b202db4b670d5
Thuan Le <Thuan Le@192.168.115.1>
2020-06-14 19:18:48 +0700

    update

---
+++ LAB1/src/main/java/CLI/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CLI;
+
+import BAI2.MainLAB1_2;
+
+/**
+ *
+ * @author Thuan Le
+ */
+public class Main {
+    public static void main(String[] args) {
+        MainLAB1_2 lab2 = new MainLAB1_2();
+    }
+  
+}
