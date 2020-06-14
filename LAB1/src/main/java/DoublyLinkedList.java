7ab92378203be19665875f3a22b3b82c225f0ccd
Admin <Admin@192.168.5.1>
2020-06-14 15:09:49 +0700

    DoublyLinkedList

--- LAB1/src/main/java/DoublyLinkedList.java
+++ LAB1/src/main/java/DoublyLinkedList.java
@@ -10,72 +10,31 @@
  */
 
 /*
- * Create class doublyLinkedList to save Player
- * Its class contains some method: 
-    - isEmpty: return size == 0, list empty
-    - insertFirst: insert Player in top of list
-    - insertLast: insert Player in last of list
-    - getFirst: return Node on the top of list
-    - getLast: return Node in last of list
-    - removeFirst: delete Node on the top of list
-    - removeLast:  delete Node in last of list
-    - removeAtPos: delete Node at any position
-    - addbetween: add new Node between two given nodes
-    - get: return value of node at specific position
+ * Create class doublyLinkedList to save Player   
  */
 public class DoublyLinkedList<E> {
     /**
      * Create inner class Node for doublyLinkedList
      *
      * @param <E>
-     */
-    private class Node<E> {
-        private E value;
-        private Node<E> next;
-        private Node<E> prev;
-        
-        public Node (E value) {
-            this.value = value;
-        }
-        //* Constructor of class Node */
-        public Node (E value, Node<E> next, Node<E> prev) {
-            this.value = value;
-            this.next = next;
-            this.prev = prev;
-        }
-        
-        public E getElement() {
-            return value;
-        }
-        
-        public Node<E> getNext() {
-            return next;
-        }
-        
-        public void setNext(Node<E> ne) {
-            next = ne;
-        }
-        
-        public Node<E> getPrev() {
-            return prev;
-        }
-        
-        public void setPrev(Node<E> pr) {
-            prev = pr;
-        }
-    }   
-    private Node<E> header;
-    private Node<E> trailer;
-    public int size = 0;
-    //* Constructor of class DoublyLinkedList */
-    public DoublyLinkedList() {
-        size = 0;
-        header = new Node(null, null, null); 
-        trailer = new Node(null, header, null);
-        header.next = trailer;
-    }
+     */  
+    private Node<E> header; 
+    private Node<E> trailer; 
+    public int size = 0; 
+
     /*
-     *Getter, Setter for Node header and trailer
+        Constructor default
+        Create Node header, trailer
+     */
+    public DoublyLinkedList() {
+        header = new Node<>(null, null, null);
+        trailer = new Node<>(null, header, null);
+        header.setNext(trailer);
+        size = 0;
+    }
+    
+    /*
+     *Create method for Setter and Getter for Node header and trailer
      */
     public Node<E> getHeader() {
         return header.getNext();
@@ -100,35 +59,40 @@ public class DoublyLinkedList<E> {
     public void setSize(int size) {
         this.size = size;
     }
-    
+      
     public boolean isEmpty() {
         return size == 0;
     }
-
     
+    /*
+     *getFirst: return Node on the top of list
+     */
     public E getFirst() {
         if (isEmpty()) {
             return null;
         }
         return header.getNext().getElement();
     }
-
-    
+    /*
+     *getLast: return Node in last of list
+     */
     public E getLast() {
         if (isEmpty()) {
             return null;
         }
         return trailer.getPrev().getElement();
     }
-
     
+    /*
+    * addbetween: add new Node between two given nodes
+    */
     public void addBetween(E e, Node<E> p, Node<E> s) {
         Node<E> newNode = new Node<>(e, p, s); 
         p.setNext(newNode);  
         s.setPrev(newNode); 
         size++; 
     }
-    
+
     private E remove(Node<E> node) {
         Node<E> p = node.getPrev();
         Node<E> s = node.getNext();
@@ -139,28 +103,34 @@ public class DoublyLinkedList<E> {
     }
 
     /*
-      *input: value of object
+     *insertFirst: insert Player in top of list
+     *input: value of object
      */
     public void insertFirst(E e) {
         addBetween(e, header, header.getNext());
     }
 
     /*
+     *insertLast: insert Player in last of list
      *input: value of object
      */
     public void insertLast(E e) {
         addBetween(e, trailer.getPrev(), trailer);
     }
-
     
+    /*
+    *removeFirst: delete Node on the top of list
+    */
     public E removeFirst() {
         if (isEmpty()) {
             return null;
         }
         return remove(header.getNext());
     }
-
-  
+    
+    /*
+    *removeLast:  delete Node in last of list
+    */
     public E removeLast() {
         if (isEmpty()) {
             return null;
@@ -169,14 +139,14 @@ public class DoublyLinkedList<E> {
     }
 
     /*
-     *input: Node location to delete
+     *removeAtPos: delete Node at any position
      */
     public void removeAtPosition(int n) {
-        if (size == 0 || n < 0) { // If size=0, there is no method to remove, if pos<0 then it will index an error index
+        if (size == 0 || n < 0) {
             return;
         }
         Node current = header.getNext(); 
-        for (int i = 0; (current != null) && (i < n); i++) { // Browse position to find
+        for (int i = 0; (current != null) && (i < n); i++) { 
             current = current.getNext();
         }
         if (current == null) {
@@ -186,8 +156,9 @@ public class DoublyLinkedList<E> {
     }
 
     /*
-     * input:  Node location to find
-     * output: Return value of Node
+     * get: return value of node at specific position
+     * input: position of node
+     * output: value of node
      */   
     public E get(int index) {
         if (size != 0) {
