36cead37fe5e6dfd4e13d27d3d6053e8b0450f92
Asus <Asus@SE140375>
2020-06-14 11:46:02 +0700

    test h

--- LAB1/src/main/java/Bai2/WritetoFile.java
+++ LAB1/src/main/java/Bai2/WritetoFile.java
@@ -18,7 +18,7 @@ import java.util.stream.Collectors;
  * @author Asus
  */
 public class WritetoFile {
-   Map<String, Integer> map= new HashMap<>();    
+    Map<String, Integer> map= new HashMap<>();    
     public void countTag(String tag) {
   
         if(map.containsKey(tag) == false) {
@@ -28,7 +28,33 @@ public class WritetoFile {
             map.put(tag, map.get(tag) + 1);
         }
     }
-    public void WriteToFile(String fileName) throws FileNotFoundException {
-    }           
+public void WriteToFile(String fileName) throws FileNotFoundException {
+    try{
+           Map<String, Integer> sortedByFrequences = map.entrySet().stream()
+            .sorted((Map.Entry.<String, Integer> comparingByValue().reversed()))
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+        
+        //Print out the map after sorting
+        System.out.println("\nList tag:");
+        for (Map.Entry<String, Integer> entry : sortedByFrequences.entrySet()) {
+            String key = entry.getKey();
+            int value = entry.getValue();
+            System.out.println("Tag:" + key +"\t\t"+ " Count: " + value);
+        }
+        File f = new File(fileName);  //Write the file
+        PrintWriter pw = new PrintWriter(f);
+        pw.println("Tag,Count\n");
+        for (Map.Entry<String, Integer> entry : sortedTag.entrySet()) {
+                String key = entry.getKey();
+                Integer value = entry.getValue();
+                pw.println(key + "," + value); 
+        }
+        pw.close();
+       }
+        catch(Exception e){
+                e.printStackTrace();
+                }
+        
+}
 }
 
