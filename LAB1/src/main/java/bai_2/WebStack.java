4efcdd2bd95ce9d9da7e3a94199d99e826b73c1e
W10 <W10@SE140293>
2020-06-14 18:55:31 +0700

    pop and push method

--- LAB1/src/main/java/bai_2/WebStack.java
+++ LAB1/src/main/java/bai_2/WebStack.java
@@ -11,5 +11,47 @@ package bai_2;
  * Build Stack structure to store Web tag
  */
 public class WebStack {
+    WebsiteInfo head,tail;//tao pointer 
+
+    public WebStack() {
+        head = tail = null;
+    }
+    public boolean isEmpty(){
+        return tail == null;
+    }
     
+    /**
+     * Them node vao , neu rong thi node la head
+     * @param tag 
+     */
+    public void push(String tag) {
+        WebsiteInfo web_tag = new WebsiteInfo(tag, head, tail);
+        if(isEmpty()) {
+            head = web_tag;
+            head.pre = null;
+            tail = web_tag;
+            tail.next = null;
+            return;
+        }
+        web_tag.pre = tail;
+        tail.next = web_tag;
+        tail = web_tag;
+        tail.next = null;
+    }
+    
+    /**
+     * Xoa node trong stack
+     */
+    public void pop() {
+        if (isEmpty()) {
+            System.err.print("Stack rong");
+        }
+        if(head == tail) {
+            head = null;
+            tail = null;
+            return;
+        }
+        tail = tail.pre;
+        tail.next = null;
+    }
 }
