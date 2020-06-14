d618270fbe013dead43426c0b45f95dcdd96f2fa
Thuan Le <Thuan Le@192.168.115.1>
2020-06-14 15:55:29 +0700

    read html

---
+++ LAB1/src/main/java/BAI2/ReadURL.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BAI2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+/**
+ *
+ * @author Thuan Le
+ */
+public class ReadURL {
+    
+    public String readHTML(String urlString) throws Exception{
+        String content = "";  
+        BufferedReader br = null;
+        try {
+            URL url = new URL(urlString);
+            br = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line;    
+            while((line = br.readLine()) != null){
+                content += line + "\n";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally{
+            try {
+                if(br != null){
+                    br.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return content;
+    }
+}
