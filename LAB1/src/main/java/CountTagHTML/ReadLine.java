d38dbb507d33765df1443aeb6428e2a97ef4418b
tcoyl98 <62016974+tcoyl98@users.noreply.github.com>
2020-06-14 11:42:38 +0700

    read content from each line

--- LAB1/src/main/java/CountTagHTML/ReadLine.java
+++ LAB1/src/main/java/CountTagHTML/ReadLine.java
@@ -5,6 +5,12 @@
  */
 package CountTagHTML;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  *
  * @author Loi Lam
@@ -12,9 +18,22 @@ package CountTagHTML;
 public class ReadLine {
     
     //
-    public String readLine(String urlWeb){
+    public String readLine(String urlWeb) throws MalformedURLException, IOException{
         String content = "";
         String line;
         int n =  0;
+        
+        //create URL with string web
+        URL url = new URL(urlWeb);
+        
+        //read content in each line from after open a connection to this URL
+        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+    
+        //read content from each line
+        while((line = br.readLine()) != null){
+            content += line + "\n";
+        }
+        br.close();
+        return content;
     }
 }
