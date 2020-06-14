b3ddfb8810f85d5bfc5ed78eafc3f592ad5e94d5
Admin <Admin@192.168.5.1>
2020-06-14 21:51:04 +0700

    CLI

--- LAB1/src/main/java/CLI/Main.java
+++ LAB1/src/main/java/CLI/Main.java
@@ -31,6 +31,35 @@ public class Main {
                 + "java -jar LAB1.jar 1 -r <<user_CSV_file>> -g <<email>>: Problem 1, get the point of user from the data strucutre\n"
                 + "java -jar LAB1.jar 1 -r <<user_CSV_file>> -t: Problem 1, get the point of the top user from the data strucutre\n");
             return;
-        }   
+        }
+   
+    // save list argument
+        String[] cmd = new String[10];
+        for (int i = 0; i < cmdStrings.length; i++) {
+            cmd[i] = cmdStrings[i];
+        }
+
+        if (cmd[0].equals("-h")) {
+            help(); 
+            return;
+        }
+
+        if (cmd[0].equals("1")) {
+            laB1(cmd);
+            return;
+        }
+
+        if (cmd[0].equals("2")) {
+            System.out.println("Not found!");
+        }
     }
-}
+    public static void help() {
+        System.out.println("Help: \n"
+                + "java -jar LAB1.jar 1 -r <<user_CSV_file>> -s <<new_user_CSV_file>>: Problem 1, read the user csv file and save the data strucutre into csv file\n"
+                + "java -jar LAB1.jar 1 -r <<user_CSV_file>> -s <<new_user_CSV_file>> -a <<email>> <<point>>: Problem 1, add a new user into the data strucutre and save to new csv file\n"
+                + "java -jar LAB1.jar 1 -r <<user_CSV_file>> -s <<new_user_CSV_file>> -d <<email>>: Problem 1, delete a user in the data strucutre and save to new csv file\n"
+                + "java -jar LAB1.jar 1 -r <<user_CSV_file>> -s <<new_user_CSV_file>> -u <<email>> <<new_point>>: Problem 1, update new point for user in the data strucutre and save to new csv file\n"
+                + "java -jar LAB1.jar 1 -r <<user_CSV_file>> -s <<new_user_CSV_file>> -dt: Problem 1, delete the top user from the data strucutre and save to new csv file\n"
+                + "java -jar LAB1.jar 1 -r <<user_CSV_file>> -g <<email>>: Problem 1, get the point of user from the data strucutre\n"
+                + "java -jar LAB1.jar 1 -r <<user_CSV_file>> -t: Problem 1, get the point of the top user from the data strucutre\n");
+    }
