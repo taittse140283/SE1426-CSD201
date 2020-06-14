7108eae5aa2a19de087c893ebc7ea4e0d2e3dfe4
ZunPeter <ZunPeter@DESKTOP-D9HH4VK>
2020-06-14 16:28:36 +0700

    get URL class

--- SE140280/src/main/java/LAB1_02/GetURL.java
+++ SE140280/src/main/java/LAB1_02/GetURL.java
@@ -5,11 +5,36 @@
  */
 package LAB1_02;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+
 /**
  *
  * @author ZunPeter
  */
 public class GetURL {
+    /*
+    * Read input and read its content
+    * @param urlString
+    * 
+    */
+    String content="";
+    String n;
     
-    
+    public String readContent(String urlString) throws Exception{
+        URL url = new URL(urlString);
+        BufferedReader bf = new BufferedReader(new InputStreamReader(url.openStream()));
+        while ((n = bf.readLine())!= null) {
+           content += n + "\n";
+            
+        }
+        bf.close();
+        return content;
+        }
+    public void printout(){
+        System.out.println(content);
+        
+    }
 }
