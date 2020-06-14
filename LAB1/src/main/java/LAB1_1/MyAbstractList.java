992b568d3bfd416b1e0ea0bd5c6d254a8d205b44
Admin <Admin@192.168.154.1>
2020-06-14 17:37:48 +0700

    big update

--- LAB1/src/main/java/LAB1_1/MyAbstractList.java
+++ LAB1/src/main/java/LAB1_1/MyAbstractList.java
@@ -12,6 +12,37 @@ package LAB1_1;
 public abstract class MyAbstractList<E> implements Stack<E>{
     public int size = 0;
     
+    /**
+     * Create Constructor
+     * A default list
+     */
     public MyAbstractList() {
+    }
+    
+    /**
+     * Add a new element at the end of this list
+     * @param e 
+     */
+    @Override
+    public void add(E e) {
+        add(size, e);
+    }
+    
+    /**
+     * Return true if this list contains no elements
+     * @return 
+     */
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    
+    /**
+     * Return the number of elements in this list
+     * @return 
+     */
+    @Override
+    public int size() {
+        return size;
     }
 }
