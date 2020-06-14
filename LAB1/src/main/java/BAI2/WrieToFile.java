36733c18077a4d0c4ec1c4f4913de40589b7e00d
Thuan Le <Thuan Le@192.168.115.1>
2020-06-14 15:47:20 +0700

    write

---
+++ LAB1/src/main/java/BAI2/WrieToFile.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BAI2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author Thuan Le
+ */
+public class WrieToFile {
+    public void writeToFile(HashMap<String, Integer> hashMap, String fileName)throws FileNotFoundException{
+        Map<String, Integer> sort = hashMap.entrySet().stream()
+                .sorted((Map.Entry.<String, Integer> comparingByValue().reversed()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+        System.out.println("After sorted");
+        
+        for(Map.Entry<String, Integer> entry : sort.entrySet()){
+            String key = entry.getKey();
+            Integer val = entry.getValue();
+            System.out.println("Tag: " + key + " Value: " + val);
+        }
+        
+        File f = new File(fileName);
+        PrintWriter pw = new PrintWriter(f);
+        pw.println("Tag,Value\n");
+        
+        for (Map.Entry<String, Integer> entry: sort.entrySet()) {
+            String key = entry.getKey();
+            Integer val = entry.getValue();
+            pw.println(key + ", " + val);
+        }
+        pw.close();
+    }
+}
