ab6e0e3a51ad17dafc939cdc42a0e6c541b111ee
tcoyl98 <62016974+tcoyl98@users.noreply.github.com>
2020-06-14 12:21:21 +0700

    create CSVFile

---
+++ LAB1/src/main/java/CountTagHTML/FileCSV.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CountTagHTML;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Loi Lam
+ */
+public class FileCSV {
+
+    Map<String, Integer> csv;
+
+    public FileCSV(){
+    csv = new HashMap<>();
+}
+}
