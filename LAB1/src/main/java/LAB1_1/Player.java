f9e55b3804fa58dc525f715d03d887bcb246ec55
Admin <Admin@192.168.154.1>
2020-06-14 17:15:54 +0700

    big update

--- LAB1/src/main/java/LAB1_1/Player.java
+++ LAB1/src/main/java/LAB1_1/Player.java
@@ -13,32 +13,29 @@ public class Player {
     private String email;
     private int point;
 
-    public Player() {
-    }
-
-    public Player(String email, int point) {
+    private Player(String email, int point) {
         this.email = email;
         this.point = point;
     }
 
-    public String getEmail() {
-        return email;
+    public static Player Create(String email, int point) {
+        return new Player(email, point);
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getPoint() {
-        return point;
+    public long getPoint() {
+        return this.point;
     }
 
     public void setPoint(int point) {
         this.point = point;
     }
-    
-    public void print(){
-        System.out.println("Email"+"\t\t"+"Point");
-        System.out.println(email+"\t\t"+point);
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    @Override
+    public String toString() {
+        return this.email + ", " + this.point;
     }
 }
