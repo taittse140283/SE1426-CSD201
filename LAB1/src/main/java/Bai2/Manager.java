56a44dc9ca755baab3eae01b1f748f359e4363dd
User <User@192.168.52.1>
2020-06-14 14:06:44 +0700

    update lan cuoi

--- LAB1/src/main/java/Bai2/Manager.java
+++ LAB1/src/main/java/Bai2/Manager.java
@@ -38,7 +38,7 @@ public class Manager {
      * @return
      * @throws java.net.MalformedURLException
      */
-    public static String readHtlmFromWeb(String urlWeb) throws MalformedURLException{
+    public static String readHtlmFromWeb(String urlWeb) throws MalformedURLException {
 
         String html = ""; // tao chuoi voi gia tri rong de luu noi dung tu web
         String line; // luu noi dung tung dong tu web size
@@ -47,7 +47,7 @@ public class Manager {
             URL url = new URL(urlWeb);
             URLConnection uRLConnection = url.openConnection();
             HttpURLConnection connection = null;
-            if(uRLConnection instanceof HttpURLConnection) {
+            if (uRLConnection instanceof HttpURLConnection) {
                 connection = (HttpURLConnection) uRLConnection;
             }
             br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
@@ -145,34 +145,87 @@ public class Manager {
         int i = html.indexOf('<'); // lay vi tri xuat hien dau tien cua ki tu '<'
         while (i != -1) { // indexOf se tra ve gia tri -1 neu khong tim thay gia tri
             int k = html.indexOf('>', i + 1); // lay vi tri xuat hien dau cua ki tu '>'
-            String tagName = html.substring(i + 1, k); // tao chuoi con
+            String tagName = html.substring(i, k + 1); // tao chuoi con
 
-            if (tagName.contains(" ")) { // the tag chua gia tri khoang trang ex: < p class="">
-                String[] s = tagName.split(" "); // tach chuoi thanh mang ki tu bang dau phan cach la khoang trang
-                tagName = s[0]; // lay gia tri dau tien trong mang tim dc
-            }
-            
-            // luu thang vao danh sach cac the dac biet
-            if (tagName.equalsIgnoreCase("area") || tagName.equalsIgnoreCase("base") || tagName.equalsIgnoreCase("br")
-                    || tagName.equalsIgnoreCase("col") || tagName.equalsIgnoreCase("!DOCTYPE") || tagName.equalsIgnoreCase("command")
-                    || tagName.equalsIgnoreCase("embed") || tagName.equalsIgnoreCase("hr") || tagName.equalsIgnoreCase("img")
-                    || tagName.equalsIgnoreCase("input") || tagName.equalsIgnoreCase("keygen") || tagName.equalsIgnoreCase("source")
-                    || tagName.equalsIgnoreCase("link") || tagName.equalsIgnoreCase("meta") || tagName.equalsIgnoreCase("param")
-                    || tagName.equalsIgnoreCase("track") || tagName.equalsIgnoreCase("wbr")) {
-                count = hashMap.containsKey(tagName) ? hashMap.get(tagName) : 0;
-                hashMap.put(tagName, count + 1);
-            } else {
-                if (!tagName.startsWith("/") && !tagName.startsWith("!")) { // neu the khong bat dau bang '/' va '!' thi luu vao danh sach va dem
-                    listTag.push(tagName);
+            if (!tagName.startsWith("</")) {// neu la the mo
+                if (!tagName.contains(" ") && !html.contains(tagName.replace("<", "</"))) { // kiem tra the the dac biet 
+
+//                    if (tagName.equals("!DOCTYPE")) {
+//                        count = hashMap.containsKey(tagName) ? hashMap.get(tagName) : 0;
+//                        hashMap.put(tagName, count + 1);
+//                        i = html.indexOf("<", k + 1);
+//                        continue;
+//                    }
+                    // co van de voi viec dem the nay bang cach xet truong hop rieng
+
+                    if (!tagName.matches("<[a-zA-Z0-9!]+>")) {// kiem tra the !-- , ngoai tru !doctype
+                        i = html.indexOf("<", k + 1);
+                        continue;
+                    }
+
                     count = hashMap.containsKey(tagName) ? hashMap.get(tagName) : 0;
                     hashMap.put(tagName, count + 1);
+                } else if (tagName.contains(" ")) { // the tag chua gia tri khoang trang ex: < p class="">
+                    String[] s = tagName.split(" ", 2); // tach chuoi thanh mang ki tu bang dau phan cach la khoang trang
+                    tagName = s[0] + ">"; // lay gia tri dau tien trong mang tim dc
+                    if (!tagName.matches("<[!a-zA-Z0-9]+>")) { // kiem tra the !-- , ngoai tru !doctype
+                        i = html.indexOf("<", k + 1);
+                        continue;
+                    }
+                    if (!html.contains(tagName.replace("<", "</"))) {
+                        count = hashMap.containsKey(tagName) ? hashMap.get(tagName) : 0;
+                        hashMap.put(tagName, count + 1);}
+                     else {
+                        if (tagName.equals("<script>")) {
+                            count = hashMap.containsKey(tagName) ? hashMap.get(tagName) : 0;
+                            hashMap.put(tagName, count + 1);
+                        } else {
+                            listTag.push(tagName);
+                        }
+                    }
+
                 } else {
-                    if (listTag.top().equalsIgnoreCase(tagName)) {
-                        listTag.pop();
+                    if (tagName.equals("<script>")) {
+                        count = hashMap.containsKey(tagName) ? hashMap.get(tagName) : 0;
+                        hashMap.put(tagName, count + 1);
+                    } else {
+                        listTag.push(tagName);
                     }
                 }
+
+            } else {
+                if (!tagName.equals("</script>")) {
+                    listTag.pop();
+                    String tag = tagName.replace("</", "<");
+                    count = hashMap.containsKey(tag) ? hashMap.get(tag) : 0;
+                    hashMap.put(tag, count + 1);
+                }
+
             }
-            i = html.indexOf('<', k + 1);
+            i = html.indexOf("<", k + 1);
+            
+            // toi uu doc the dac biet
+            // luu thang vao danh sach cac the dac biet
+//            if (tagName.equalsIgnoreCase("area") || tagName.equalsIgnoreCase("base") || tagName.equalsIgnoreCase("br")
+//                    || tagName.equalsIgnoreCase("col") || tagName.equalsIgnoreCase("!DOCTYPE") || tagName.equalsIgnoreCase("command")
+//                    || tagName.equalsIgnoreCase("embed") || tagName.equalsIgnoreCase("hr") || tagName.equalsIgnoreCase("img")
+//                    || tagName.equalsIgnoreCase("input") || tagName.equalsIgnoreCase("keygen") || tagName.equalsIgnoreCase("source")
+//                    || tagName.equalsIgnoreCase("link") || tagName.equalsIgnoreCase("meta") || tagName.equalsIgnoreCase("param")
+//                    || tagName.equalsIgnoreCase("track") || tagName.equalsIgnoreCase("wbr") || tagName.equalsIgnoreCase("script")) {
+//                count = hashMap.containsKey(tagName) ? hashMap.get(tagName) : 0;
+//                hashMap.put(tagName, count + 1);
+//            } else {
+//                if (!tagName.startsWith("/") && !tagName.startsWith("!")) { // neu the khong bat dau bang '/' va '!' thi luu vao danh sach va dem
+//                    listTag.push(tagName);
+//                    count = hashMap.containsKey(tagName) ? hashMap.get(tagName) : 0;
+//                    hashMap.put(tagName, count + 1);
+//                } else {
+//                    if (listTag.top().equalsIgnoreCase(tagName)) {
+//                        listTag.pop();
+//                    }
+//                }
+//            }
+//            i = html.indexOf('<', k + 1);
         }
     }
 }
