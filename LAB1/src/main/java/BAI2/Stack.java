a2bf630260fd70d76b1f53352390907849b611f3
Thuan Le <Thuan Le@192.168.115.1>
2020-06-14 13:17:34 +0700

    update

--- LAB1/src/main/java/BAI2/Stack.java
+++ LAB1/src/main/java/BAI2/Stack.java
@@ -9,39 +9,12 @@ package BAI2;
  *
  * @author Thuan Le
  */
-public interface Stack<E> {
-    /**
-     * Return the number of elements in the stack
-     * @return number of elements in the stack
-     */
-    int size();
+public class Stack {
+   TagOfHTML header, trailer;
+
+    public Stack() {
+        header = trailer = null;
+    }
     
-    /**
-     * Test whether the stack is empty
-     * @return true if the stack is empty, false otherwise
-     */
-    boolean isEmpty();
-    
-    /**
-     * Insert an element at the top of the stack
-     * @param element   the element to be inserted
-     */
-    void push(E element);
-    
-    /**
-     * Returns, but doesn't remove, the element at the top of the stack
-     * @return top element in the stack ( or null if stack is empty)
-     */
-    E top();
-    
-    /**
-     * Removes and return the top element from the stack
-     * @return element removed (or null if stack is empty)
-     */
-    E pop();
-    
-    /**
-     * Display info of stack
-     */
-    void print();
+   
 }
