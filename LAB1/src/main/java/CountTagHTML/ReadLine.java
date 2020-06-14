fea2a0d97e790a7f4cbaa547d0e6985742f99e39
tcoyl98 <62016974+tcoyl98@users.noreply.github.com>
2020-06-14 11:34:10 +0700

    create param

---
+++ LAB1/src/main/java/CountTagHTML/ReadLine.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CountTagHTML;
+
+/**
+ *
+ * @author Loi Lam
+ */
+public class ReadLine {
+    
+    //
+    public String readLine(String urlWeb){
+        String content = "";
+        String line;
+        int n =  0;
+    }
+}
