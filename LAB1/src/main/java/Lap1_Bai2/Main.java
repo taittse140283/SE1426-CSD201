0deeef883b8d661c7301e8e458ff8a787af67dfa
HoangMinhFpt <65846831+HoangMinhFpt@users.noreply.github.com>
2020-06-14 12:01:47 +0700

    update something function

---
+++ LAB1/src/main/java/Lap1_Bai2/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lap1_Bai2;
+
+/**
+ *
+ * @author PC
+ */
+public class Main {
+    public static void main(String[] args) {
+        HTMLCount tag = new HTMLCount();
+        tag.manage("outputHTML.csv","VnExpress1.html");
+    }
+}
