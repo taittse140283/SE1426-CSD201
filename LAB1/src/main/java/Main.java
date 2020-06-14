edf43ff58857bb7da1f7da6009ae2899be7d0c86
ealflm <ealflm@192.168.58.1>
2020-06-14 14:05:22 +0700

    some new update

--- LAB1/src/main/java/Main.java
+++ LAB1/src/main/java/Main.java
@@ -2,6 +2,8 @@
  *
  * @author SE140834
  */
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import problem1.*;
 import problem2.*;
 
@@ -9,8 +11,11 @@ public class Main {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         Problem1 problem1 = new Problem1();
+        problem1.readFile("user.csv");
+        problem1.writeFile("output.csv");
     }
 }
