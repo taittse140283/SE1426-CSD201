b19e981864ec7a06bb6b592dc4ff6ed0828b08aa
Thuan Le <Thuan Le@192.168.115.1>
2020-06-14 19:01:57 +0700

    update

---
+++ LAB1/src/main/java/BAI2/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BAI2;
+
+/**
+ *
+ * @author Thuan Le
+ */
+public class Main {
+    public static void main(String[] args) throws Exception{
+        String url = "https://www.facebook.com/?ref=tn_tnmn";
+        String file = "outputlab2.csv";
+        Manager run = new Manager();
+        run.runLab2(url, file);
+        System.out.println("");
+    }
+    
+}
