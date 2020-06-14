6c1f067566b12219924abdfe7f0516cd34cc43ad
luongse <luonghb24@gmail.com>
2020-06-14 09:14:14 +0700

    add class main

---
+++ LAB1/src/main/java/bai2/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai2;
+
+/**
+ *
+ * @author Admin
+ */
+public class Main {
+     public static void main(String[] args) throws Exception {
+        Manager manager = new Manager();
+        String url = "https://www.stylecraze.com/articles/most-beautiful-flowers-in-the-world/";
+        String file = "output1.csv";
+        
+}
