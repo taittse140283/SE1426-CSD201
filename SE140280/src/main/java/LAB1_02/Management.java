52f01754b0960c2cb8f01f5d82db89190d1f20cb
ZunPeter <ZunPeter@DESKTOP-D9HH4VK>
2020-06-14 15:42:00 +0700

    Management CLass

---
+++ SE140280/src/main/java/LAB1_02/Management.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LAB1_02;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author ZunPeter
+ */
+public class Management {
+   
+}
