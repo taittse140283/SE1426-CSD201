e67d248e365fe93bb8495837766edd128d1bfbb4
Linh <Linh@SE141095.mshome.net>
2020-06-14 19:34:15 +0700

    update...

--- LAB1/src/main/java/Main.java
+++ LAB1/src/main/java/Main.java
@@ -48,6 +48,8 @@ public class Main {
                     else
                         System.out.println("Not valid argument!");
                 }
+                else
+                    System.out.println("Not valid argument!");
             }
         }
         catch(Exception e) {
