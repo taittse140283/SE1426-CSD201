3d9026879611b81bd80280eea15f933ee6a16e39
Admin <Admin@192.168.154.1>
2020-06-14 19:33:39 +0700

    update add, delete

--- LAB1/src/main/java/LAB1_1/PriorityQueue.java
+++ LAB1/src/main/java/LAB1_1/PriorityQueue.java
@@ -11,5 +11,27 @@ package LAB1_1;
  * @author Admin
  */
 public class PriorityQueue{
+    private DoublyLinkedList<Player> list = new DoublyLinkedList<>();
     
+    public PriorityQueue(){
+        
+    }
+    
+    public void addPlayer(Player data){
+        for (int i = 0; i < list.size(); i++) {
+            int index = list.size() - i -1 ;
+            if (data.getPoint()<= this.list.get(index).getPoint()){
+                this.list.add(index+1, data);
+                return;
+            }
+        }
+    }
+    
+    public void deletePlayer(String email){
+        for (int i = 0; i < list.size(); i++) {
+            if(this.list.get(i).getEmail().equals(email)){
+                this.list.remove(i);
+            }
+        }
+    }
 }
