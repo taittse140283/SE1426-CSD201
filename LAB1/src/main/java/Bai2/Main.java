011daf4e1041cb92150ca7fda64e192a1790b1e2
User <User@192.168.52.1>
2020-06-14 14:07:24 +0700

    update

--- LAB1/src/main/java/Bai2/Main.java
+++ LAB1/src/main/java/Bai2/Main.java
@@ -3,24 +3,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Bai2;
-
-import java.net.MalformedURLException;
-import java.util.HashMap;
-
-/**
- *
- * @author User
- */
-public class Main {
-
-    public static void main(String[] args) throws MalformedURLException {
-        HashMap<String, Integer> hashMap = new HashMap<>();
-        String url = "https://vnexpress.net/?fbclid=IwAR22hMxE_-zTckjENlmvtCj3fJMpkCERIs4vab4iZO3wAsLvGIwOvOSoco8";
-        String output = "outputTest.csv";
-
-            // test
-        Manager.process(hashMap, Manager.readHtlmFromWeb(url));
-        Manager.writeToFile(hashMap, output);
-    }
-}
+//package Bai2;
+//
+//import java.net.MalformedURLException;
+//import java.util.HashMap;
+//
+///**
+// *
+// * @author User
+// */
+//public class Main {
+//
+//    public static void main(String[] args) throws MalformedURLException {
+//        HashMap<String, Integer> hashMap = new HashMap<>();
+//        String url = "https://vnexpress.net/?fbclid=IwAR22hMxE_-zTckjENlmvtCj3fJMpkCERIs4vab4iZO3wAsLvGIwOvOSoco8";
+//        String output = "outputTest.csv";
+//
+//            // test
+//        Manager.process(hashMap, Manager.readHtlmFromWeb(url));
+//        Manager.writeToFile(hashMap, output);
+//    }
+//}
