9e51fbf849a4552501e52500c84430e3a37f7eee
Admin <Admin@192.168.154.1>
2020-06-14 18:36:43 +0700

    update removeLast(), remove()

--- LAB1/src/main/java/LAB1_1/DoublyLinkedList.java
+++ LAB1/src/main/java/LAB1_1/DoublyLinkedList.java
@@ -111,7 +111,56 @@ public class DoublyLinkedList<E> extends MyAbstractList<E>{
             return temp.element;
         }
     }
-        
+    /**
+     * Remove the last node and
+     * @return return the object that is contained in the removed node.
+     */
+    public E removeLast() {
+        if (size == 0) {
+            return null;
+        } else if (size == 1) {
+            Node<E> temp = head;
+            head = tail = null;
+            size = 0;
+            return temp.element;
+        } else {
+            Node<E> current = head;
+            for (int i = 0; i < size - 2; i++) {
+            current = current.next;
+            }
+            Node<E> temp = tail;
+            tail = current;
+            tail.next = null;
+            size--;
+            return temp.element;
+        }
+    }
+    
+    /**
+     * Remove the element at the specified position in this list.
+     * @param index
+     * @return Return the element that was removed from the list. 
+     */
+    @Override
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        } else if (index == 0) {
+            return removeFirst();
+        } else if (index == size - 1) {
+            return removeLast();
+        } else {
+            Node<E> previous = head;
+            for (int i = 1; i < index; i++) {
+            previous = previous.next;
+            }
+        Node<E> current = previous.next;
+        previous.next = current.next;
+        size--;
+        return current.element;
+        }
+    }
+    
     @Override
     public void clear() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -139,11 +188,6 @@ public class DoublyLinkedList<E> extends MyAbstractList<E>{
 
     @Override
     public boolean remove(E e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public E remove(int index) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
