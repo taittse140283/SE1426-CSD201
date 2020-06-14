ea0d395f37640b325bc7a2f31db7d9e95c44a4ba
Hong Son <hongson2992000@gmail.com>
2020-06-14 21:22:38 +0700

    fix class DoubleLinkedList and update new code

--- LAB1/src/main/java/DoubleLinkedList.java
+++ LAB1/src/main/java/DoubleLinkedList.java
@@ -3,61 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Admin
  */
-public class DoubleLinkedList<Gamer> {
-    //Create class Node
+public class DoubleLinkedList {
+    
 
-    private class Node {
-
-        private Gamer data;
-        private Node start;
-        private Node end;
-        //Create contructors 
-
-        public Node(Gamer data, Node start, Node end) {
-            this.data = data;
-            this.start = start;
-            this.end = end;
-        }
-
-        // Create getter and setter
-        public Gamer getData() {
-            return data;
-        }
-
-        public void setData(Gamer data) {
-            this.data = data;
-        }
-
-        public Node getStart() {
-            return start;
-        }
-
-        public void setStart(Node start) {
-            this.start = start;
-        }
-
-        public Node getEnd() {
-            return end;
-        }
-
-        public void setEnd(Node end) {
-            this.end = end;
-        }
-
-    }
-    private Node header;
-    private Node trailer;
-    public int sizeOfList = 0;
+    Gamer head;
+    Gamer tail;
 
     public DoubleLinkedList() {
-        header = new Node(null, null, null);
-        trailer = new Node(null,null,header);
-        header.start= trailer;
+        head = tail = null;
     }
 
+      
 }
