bb10b68273afd98d1e3b910d780522f6cee02679
W10 <W10@SE140293>
2020-06-14 18:07:41 +0700

    ADD comment for WebsiteInfo class

--- LAB1/src/main/java/bai_2/WebsiteInfo.java
+++ LAB1/src/main/java/bai_2/WebsiteInfo.java
@@ -8,7 +8,44 @@ package bai_2;
 /**
  *
  * @author W10
+ * This class contains basic information of input's HTML web
  */
 public class WebsiteInfo {
+    private String tag;
+    WebsiteInfo next,pre;//tao pointer cua chinh no
+
+    /*
+        construtor,getter,setter of html node
+    */
+    public WebsiteInfo(String tag, WebsiteInfo next, WebsiteInfo pre) {
+        this.tag = tag;
+        this.next = next;
+        this.pre = pre;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public WebsiteInfo getNext() {
+        return next;
+    }
+
+    public void setNext(WebsiteInfo next) {
+        this.next = next;
+    }
+
+    public WebsiteInfo getPre() {
+        return pre;
+    }
+
+    public void setPre(WebsiteInfo pre) {
+        this.pre = pre;
+    }
+    
     
 }
