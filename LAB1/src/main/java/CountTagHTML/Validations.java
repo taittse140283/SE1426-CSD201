c49da14d67827ced32d0c8d503c5328a764b6e46
tcoyl98 <62016974+tcoyl98@users.noreply.github.com>
2020-06-14 15:36:49 +0700

    add try catch

--- LAB1/src/main/java/CountTagHTML/Validations.java
+++ LAB1/src/main/java/CountTagHTML/Validations.java
@@ -10,17 +10,23 @@ import java.io.IOException;
 /**
  *
  * @author Loi Lam
- */ 
+ */
 public class Validations {
+
     ReadLine rl = new ReadLine();
     FileCSV csv = new FileCSV();
     Stack s = new Stack();
-    
-    //read line of urlWeb, print and sort
-    public Validations(String urlWeb, String fileName) throws IOException{
-        String content = rl.readLine(urlWeb);
-        System.out.println("Processing push and pop the tag in stack");
-        csv.print();
-        csv.sortAndPrint(fileName);
+
+    //read line of urlWeb, sort and write to file
+    public Validations(String urlWeb, String fileName) throws IOException {
+        try {
+            String content = rl.readLine(urlWeb);
+            System.out.println("Processing push and pop the tag in stack");
+            csv.print();
+            csv.sortAndPrint(fileName);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
