9084aad63a3ac728e48ac1bd4da7f2f397e3122a
hiep <hiep@192.168.56.1>
2020-06-14 11:40:28 +0700

    isEmpty ham

---
+++ LAB1/src/main/java/Lab1_2/Stack.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab1_2;
+
+/**
+ *
+ * @author hiep
+ */
+public class Stack {
+    Node head,tail;
+
+    public Stack() {
+        head=null;
+        tail=null;
+    }
+    public boolean isEmpty(){
+        return tail==null;
+    }
+
+    
+}
