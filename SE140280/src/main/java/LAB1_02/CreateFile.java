16c6b250dcb9033fd1d576e4d487ed937c555e7d
ZunPeter <ZunPeter@DESKTOP-D9HH4VK>
2020-06-16 13:30:17 +0700

    Finish Create File

--- SE140280/src/main/java/LAB1_02/CreateFile.java
+++ SE140280/src/main/java/LAB1_02/CreateFile.java
@@ -5,7 +5,9 @@
  */
 package LAB1_02;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -20,5 +22,19 @@ public class CreateFile {
       Map<String, Integer> sort = hashmap.entrySet().stream().sorted((Map.Entry.<String, Integer> comparingByValue().reversed()))
           .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
        System.out.println("\n After sort :");
+       for (Map.Entry<String, Integer> entry : sort.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            System.out.println("Tag: " + key + " Value: " + value);
+        }
+       File f = new File(fileName);
+       PrintWriter print = new PrintWriter(f);
+       print.println("\n Tag,Value ");
+       for (Map.Entry<String, Integer> entry : sort.entrySet()) {
+           String key = entry.getKey();
+           Integer value = entry.getValue();
+           print.println(key +" "+ value);
+       }
+       print.close();
    } 
 }
