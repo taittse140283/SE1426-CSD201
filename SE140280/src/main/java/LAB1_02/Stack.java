7da6bbf2c62c4b27f431ab5a09716bc40989b68d
ZunPeter <ZunPeter@DESKTOP-D9HH4VK>
2020-06-16 13:39:17 +0700

    update all comment

--- SE140280/src/main/java/LAB1_02/Stack.java
+++ SE140280/src/main/java/LAB1_02/Stack.java
@@ -17,19 +17,22 @@ public class Stack {
     head = null;
     tail = null;
     }
-    /*return the number of elements in the stack
-    @return number of element in stack*/
+    /**return the number of elements in the stack
+    * @return number of element in stack
+    */
     private int size = 0;
     int size(){
       return size;
     }
-    /*Check if stack is Empty
-    @return true if the stack is empty */
+    /**Check if stack is Empty
+    * @return true if the stack is empty 
+    */
     public boolean isEmpty(){
        return size == 0; 
     }
-    /*Insert element in top of the stack
-    @return top element in the stack or null if empty*/
+    /**Insert element in top of the stack
+    * @return top element in the stack or null if empty
+    */
     public void push(String tagHTML){
         NodeHTML tag= new NodeHTML(tagHTML, head, tail);
         /*check if stack is empty*/
