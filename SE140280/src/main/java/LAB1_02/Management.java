6eec3b087f2d51d4f60424a2a8d101d6be703df3
ZunPeter <ZunPeter@DESKTOP-D9HH4VK>
2020-06-16 14:06:16 +0700

    processTag

--- SE140280/src/main/java/LAB1_02/Management.java
+++ SE140280/src/main/java/LAB1_02/Management.java
@@ -17,7 +17,55 @@ public class Management {
    Stack stack = new Stack();
    CreateFile cre = new CreateFile();
    
-   /** Check all tags in the HTML base on  '<','>' 
+   public void processTag(String tag) {
+        
+        if (tag.equalsIgnoreCase("<area>")) {
+            count(tag);
+        } else if (tag.equalsIgnoreCase("<!DOCTYPE>")) {
+            count(tag); 
+        } else if (tag.equalsIgnoreCase("<input>")) {
+            count(tag);
+        } else if (tag.equalsIgnoreCase("<link>")) {
+            count(tag);
+        } else if (tag.equalsIgnoreCase("<base>")) {
+            count(tag);
+        } else if (tag.equalsIgnoreCase("<br>")) {
+            count(tag);
+        } else if (tag.equalsIgnoreCase("<command>")) {
+            count(tag);
+        } else if (tag.equalsIgnoreCase("<track>")) {
+            count(tag);
+        } else if (tag.equalsIgnoreCase("<wbr>")) {
+            count(tag);
+        } else if (tag.equalsIgnoreCase("<embeb>")) {
+            count(tag);
+        } else if (tag.equalsIgnoreCase("<hr>")) {
+            count(tag);
+        } else if (tag.equalsIgnoreCase("<!-")) {
+            count("<!--comment-->");
+        } else if (tag.equalsIgnoreCase("<img>")) {
+            count(tag);
+        } else if (tag.equalsIgnoreCase("<meta>")) {
+            count(tag);
+        } else if (tag.equalsIgnoreCase("<param>")) {
+            count(tag);
+        } else {
+            
+            if (tag.contains("</") == false) {
+                stack.push(tag);   
+                count(tag);
+                stack.print();
+                System.out.println();
+            } 
+            else if (stack.top().equalsIgnoreCase(tag.replace("/", "")) == true) {
+                stack.pop();
+                stack.print();
+                System.out.println();
+            }
+        }
+}
+   
+   /** Check all tags in the HTML base on  '<','>' and '-'
     * @param content  
     */
    public void readTag(String content){
