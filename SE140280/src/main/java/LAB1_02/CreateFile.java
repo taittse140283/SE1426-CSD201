6be1569d02e60fe65d1a34ad52fb0e92f1dd9c35
ZunPeter <ZunPeter@DESKTOP-D9HH4VK>
2020-06-16 13:21:41 +0700

    Create File

--- SE140280/src/main/java/LAB1_02/CreateFile.java
+++ SE140280/src/main/java/LAB1_02/CreateFile.java
@@ -5,10 +5,20 @@
  */
 package LAB1_02;
 
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 /**
  *
  * @author ZunPeter
  */
 public class CreateFile {
-    
+   public void CreateFile(HashMap<String, Integer> hashmap,String fileName)throws FileNotFoundException{
+      Map<String, Integer> sort = hashmap.entrySet().stream().sorted((Map.Entry.<String, Integer> comparingByValue().reversed()))
+          .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+       System.out.println("\n After sort :");
+   } 
 }
